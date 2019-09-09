847bccf497baa22a8ab21f69042a737605c3ed45
tracer-ics2019 <tracer@njuics.org>
2019-09-09 15:49:17 +0800

    >  compile 181250195 zyf Linux zyf 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  15:49:17 up  1:32,  3 users,  load average: 0.04, 0.05, 0.04 2af9c64480ed2a717387b68bb82be5d43263b59

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -36,6 +36,20 @@ static int cmd_q(char *args) {
   return -1;
 }
 
+static int cmd_si(char *args){
+  char *arg = strtok(NULL, " ");
+  if (arg == NULL){
+		/* no arguments given, with default value 1 */
+		cpu_exec(1);	
+  }
+  else{
+		int num = atoi(arg);
+		assert(num);
+		cpu_exec(num);
+  }
+  return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -46,6 +60,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si","Usage: si[n]\nStep N instructions.",cmd_si },
 
   /* TODO: Add more commands */
 
