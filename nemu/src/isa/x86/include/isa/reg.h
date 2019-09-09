4313b47591ecb35b0b8357bd2d2989f6bfdf56a6
tracer-ics2019 <tracer@njuics.org>
2019-09-09 08:09:45 +0800

    >  compile 181250195 zyf Linux zyf 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  08:09:45 up 14 min,  1 user,  load average: 0.91, 0.92, 0.67 53d6cc05b51d9e353f38defe582c8afad77dbc0

--- nemu/src/isa/x86/include/isa/reg.h
+++ nemu/src/isa/x86/include/isa/reg.h
@@ -17,14 +17,16 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef struct {
-  union{
-  struct {rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;};
-  union {
-	uint32_t _32;
-	uint16_t _16;
-	uint8_t _8[2];
-  } gpr[8];
-  };
+	union{
+		struct {
+			rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+		};
+		union {
+			uint32_t _32;
+			uint16_t _16;
+			uint8_t _8[2];
+		} gpr[8];
+  	};
 
   /* Do NOT change the order of the GPRs' definitions. */
 
