b94ebf5f22338a896b962339566a8a3494dca7df
tracer-ics2019 <tracer@njuics.org>
2019-09-10 12:42:38 +0800

    >  compile 181250195 zyf Linux zyf 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  12:42:38 up 8 min,  1 user,  load average: 0.46, 1.18, 0.80 4f7b1e2625edd9705d322b55d2a2ba7a1644990d

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -86,8 +86,8 @@ static int cmd_x(char *args){
 		if(!times || !addr_head) printf("Usage: x [n][address]\n");
 		else{
 			uint32_t i;
-			for(i=0;i<times;i+=4){
-				printf("0x%x: 0x%08x\n",addr_head+i,vaddr_read(addr_head+i,4));
+			for(i=0;i<times;i++){
+				printf("0x%x: 0x%08x\n",addr_head+4*i,vaddr_read(addr_head+i,4));
 			}
 		}
   }
