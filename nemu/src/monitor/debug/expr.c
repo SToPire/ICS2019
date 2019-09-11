e2e46464125be2133e3c6b9ea9015488923f1cc1
tracer-ics2019 <tracer@njuics.org>
2019-09-11 20:34:40 +0800

    >  compile 181250195 zyf Linux zyf 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  20:34:40 up  2:33,  4 users,  load average: 0.00, 0.03, 0.06 3c4f483ec721ea88ae54d60161c85306959b1b02

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -87,7 +87,10 @@ static bool make_token(char *e) {
 
         switch (rules[i].token_type) {
 			case TK_NOTYPE: break;
-			case '+': tokens[nr_token].type='+';break;
+			case '+': tokens[nr_token].type='+';
+					  strcpy(tokens[nr_token].str,"");
+					  ++nr_token;
+					  break;
 			default: TODO();
         }
 
