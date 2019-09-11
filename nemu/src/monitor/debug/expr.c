9a637bdea59bf350ca9c4e9d88d0fe2136b2d2a3
tracer-ics2019 <tracer@njuics.org>
2019-09-11 21:42:04 +0800

    >  compile 181250195 zyf Linux zyf 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:42:04 up  3:40,  4 users,  load average: 0.30, 0.18, 0.12 a8455ba3e617c5b8b628051fac5e2c95e313468

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -86,7 +86,7 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-
+printf("%d",rules[i].token_type);
         switch (rules[i].token_type) {
 			case TK_NOTYPE: break;
 			case '+': tokens[nr_token].type='+';
