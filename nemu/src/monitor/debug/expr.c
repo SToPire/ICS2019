4e03884af8639f8b852dba32366ffc5a685008d6
tracer-ics2019 <tracer@njuics.org>
2019-09-17 17:19:17 +0800

    >  compile 181250195 zyf Linux zyf 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  17:19:17 up  3:13,  1 user,  load average: 0.50, 0.52, 0.33 4ffc5da762dc19adc32bdf4da1ba5222a6e5dcc

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -205,10 +205,10 @@ uint32_t eval(int p,int q){
 			uint32_t val1=eval(p,op-1);
 			uint32_t val2=eval(op+1,q);
 			switch(tokens[op].type){
-				case '+':return (uint32_t)(val1 + val2);
-				case '-':return (uint32_t)(val1 - val2);
-				case '*':return (uint32_t)(val1 * val2);
-				case '/':return (uint32_t)(val1 / val2);
+				case '+':return (uint32_t)((uint32_t)val1 + val2);
+				case '-':return (uint32_t)((uint32_t)val1 - val2);
+				case '*':return (uint32_t)((uint32_t)val1 * val2);
+				case '/':return (uint32_t)((uint32_t)val1 / val2);
 			}
 		}
 	}
