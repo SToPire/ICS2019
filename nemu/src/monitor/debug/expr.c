793b6c4bab869cd9c945b4f8e0f427daf2b5b6e4
tracer-ics2019 <tracer@njuics.org>
2019-09-17 18:10:03 +0800

    >  compile 181250195 zyf Linux zyf 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  18:10:03 up  4:03,  1 user,  load average: 0.45, 0.19, 0.10 bd055dbc20b2ccc99d1a7dbe8ab8b67d90d7a143

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -215,6 +215,7 @@ uint32_t eval(int p,int q){
 				case '/':return (uint32_t)((uint32_t)val1 / val2);
 			}
 		}
+printf("%d",v);
 	}
 	return 23333; //will not be executed forever, just for pass the compliation.
 }
