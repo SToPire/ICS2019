bb22ca61fc74e520e3b5ade0e421a20479651f0a
tracer-ics2019 <tracer@njuics.org>
2019-09-20 21:45:42 +0800

    >  compile 181250195 zyf Linux zyf 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:45:42 up  1:51,  1 user,  load average: 0.70, 0.58, 0.45 2b4833812834bd2ef441748fa39dfdfe45a280ac

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -277,7 +277,8 @@ uint32_t expr(char *e, bool *success) {
   int i;
   for(i=0;i<nr_token;i++){
   	if(tokens[i].type==TK_REG){
-  		uint32_t t=isa_reg_str2val(tokens[i].str,success);printf("HERE\n");
+  	printf("HERE\n");
+  		uint32_t t=isa_reg_str2val(tokens[i].str,success);
   		if(*success){tokens[i].type=TK_NUM;sprintf(tokens[i].str,"%u",t);}
   		else return 0;
   	}
