a64f89c91dc8e1b5db20ea65424fddad1ed8444c
tracer-ics2019 <tracer@njuics.org>
2019-09-21 14:59:06 +0800

    >  compile 181250195 zyf Linux zyf 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  14:59:06 up  4:55,  1 user,  load average: 0.84, 0.41, 0.31 2167c27c4d743f61fceb7321fc487b8100e99bc

--- nemu/src/isa/x86/reg.c
+++ nemu/src/isa/x86/reg.c
@@ -55,7 +55,8 @@ uint32_t isa_reg_str2val(const char *s, bool *success) {
 	int i,l=strlen(s);
 	*success = true;
 	char tmp[10]="";strcpy(tmp,s);
-	for(i=0;i<l;i++) tmp[i]=tolower(s[i]); 
+	for(i=0;i<l;i++) tmp[i]=tolower(s[i]);
+	if(strcmp(tmp,"pc")==0) return cpu.pc; 
 	for(i=0;i<8;i++){
 		if(strcmp(tmp,reg_name(i,1))==0) return reg_b(i);
 		if(strcmp(tmp,reg_name(i,2))==0) return reg_w(i);
