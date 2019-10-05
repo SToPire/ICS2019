32cd0298246a1885a6bacae4de866bd064c4eb28
tracer-ics2019 <tracer@njuics.org>
2019-10-05 10:26:12 +0800

    >  compile 181250195 zyf Linux zyf 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  10:26:12 up  2:15,  1 user,  load average: 0.88, 0.54, 0.42 4f3d3beab5726ec403137a84bd3f6e145a032

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -137,7 +137,9 @@ static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+ 	int w=8*(4-width);
+ 	*dest = *src1 << w;
+ 	*dest = (rtlreg_t)((int32_t)*dest >> w);
 }
 
 static inline void rtl_setrelopi(uint32_t relop, rtlreg_t *dest,
