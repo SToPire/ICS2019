0febb1fae8dfeb6a58fcb08931a8510fcda32e11
tracer-ics2019 <tracer@njuics.org>
2019-10-17 20:28:11 +0800

    >  compile 181250195 zyf Linux zyf 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  20:28:11 up  3:33,  1 user,  load average: 0.48, 0.32, 0.39 b2c7ea77232b2152f8cb81d1c87ee84f4a59de61

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -27,7 +27,8 @@ make_EHelper(sub) {
 }
 
 make_EHelper(cmp) { 
-	rtl_sext(&s2,&id_src->val,id_src->width);printf("%x\n",s2); printf("%x\n",id_dest->val);   
+  if(id_src->width != id_dest->width)
+	  rtl_sext(&s2,&id_src->val,id_src->width);   
   rtl_sub(&s0,&id_dest->val,&s2);printf("%d\n",s0);
   rtl_is_sub_overflow(&s1,&s0,&id_dest->val,&s2,id_dest->width);
   rtl_set_OF(&s1);
