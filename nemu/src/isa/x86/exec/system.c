ea5eea876c9296809deaca5a7e8bac36ad0a5a8e
tracer-ics2019 <tracer@njuics.org>
2019-10-21 00:05:25 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-31-generic #33~18.04.1-Ubuntu SMP Tue Oct 1 10:20:39 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  00:05:25 up 56 min,  1 user,  load average: 0.56, 0.58, 0.65 636337f1734df6b06612f6e49d98f00032533aba

--- nemu/src/isa/x86/exec/system.c
+++ nemu/src/isa/x86/exec/system.c
@@ -56,7 +56,7 @@ make_EHelper(in) {
 }
 
 make_EHelper(out) {
-  if(id_src->width==1){printf("FUCK\n");
+  if(id_src->width==1){printf("dest:%x src:%x\n",id_dest->val,id_src->val);
     pio_write_b(id_dest->val,id_src->val);
   }
   else if(id_src->width==2){
