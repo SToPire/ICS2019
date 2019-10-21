bc87df5553576f9396b3a9a11539d6b4c4fe65b2
tracer-ics2019 <tracer@njuics.org>
2019-10-21 08:45:09 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-31-generic #33~18.04.1-Ubuntu SMP Tue Oct 1 10:20:39 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  08:45:09 up 30 min,  1 user,  load average: 0.73, 0.76, 0.68 2b350cb3a2f53641ef76ae871cd698754321063f

--- nemu/src/isa/x86/exec/system.c
+++ nemu/src/isa/x86/exec/system.c
@@ -1,70 +1,74 @@
 #include "cpu/exec.h"
 
-make_EHelper(lidt) {
-  TODO();
+make_EHelper(lidt)
+{
+    TODO();
 
-  print_asm_template1(lidt);
+    print_asm_template1(lidt);
 }
 
-make_EHelper(mov_r2cr) {
-  TODO();
+make_EHelper(mov_r2cr)
+{
+    TODO();
 
-  print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
+    print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
-make_EHelper(mov_cr2r) {
-  TODO();
+make_EHelper(mov_cr2r)
+{
+    TODO();
 
-  print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
+    print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
 
-  difftest_skip_ref();
+    difftest_skip_ref();
 }
 
-make_EHelper(int) {
-  TODO();
+make_EHelper(int)
+{
+    TODO();
 
-  print_asm("int %s", id_dest->str);
+    print_asm("int %s", id_dest->str);
 
-  difftest_skip_dut(1, 2);
+    difftest_skip_dut(1, 2);
 }
 
-make_EHelper(iret) {
-  TODO();
+make_EHelper(iret)
+{
+    TODO();
 
-  print_asm("iret");
+    print_asm("iret");
 }
 
 uint32_t pio_read_l(ioaddr_t);
 uint32_t pio_read_w(ioaddr_t);
 uint32_t pio_read_b(ioaddr_t);
-void pio_write_l(ioaddr_t, uint32_t);
-void pio_write_w(ioaddr_t, uint32_t);
-void pio_write_b(ioaddr_t, uint32_t);
+void     pio_write_l(ioaddr_t, uint32_t);
+void     pio_write_w(ioaddr_t, uint32_t);
+void     pio_write_b(ioaddr_t, uint32_t);
 
-make_EHelper(in) {
-  if(id_src->width==1){
-    s0 = pio_read_b(id_src->val);
-  }
-  else if(id_src->width==2){
-    s0 = pio_read_w(id_src->val);
-  }
-  else{
-    s0 = pio_read_l(id_src->val);
-  }
-  operand_write(id_dest,&s0);
-  print_asm_template2(in);
+make_EHelper(in)
+{
+    if (id_src->width == 1) {
+        s0 = pio_read_b(id_src->val);
+    } else if (id_src->width == 2) {
+        s0 = pio_read_w(id_src->val);
+    } else {
+        s0 = pio_read_l(id_src->val);
+    }
+    operand_write(id_dest, &s0);
+    print_asm_template2(in);
 }
 
-make_EHelper(out) {
-  if(id_src->width==1){printf("dest:%x src:%x\n",id_dest->val,id_src->val);
-    pio_write_b(id_src->val,id_src->val);
-  }
-  else if(id_src->width==2){
-    pio_write_w(id_dest->val,id_src->val);
-  } 
-  else{
-    pio_write_l(id_dest->val,id_src->val);
-  }
+make_EHelper(out)
+{
+    if (id_src->width == 1) {
+        printf("dest:%x src:%x\n", id_dest->val, id_src->val);
+        pio_write_b(id_dest->val, id_src->val);
+    } else if (id_src->width == 2) {
+        pio_write_w(id_dest->val, id_src->val);
+    } else {
+        pio_write_l(id_dest->val, id_src->val);
+    }
 
-  print_asm_template2(out);
+    print_asm_template2(out);
 }
--- nemu/src/main.c
+++ nemu/src/main.c
@@ -2,10 +2,10 @@ int init_monitor(int, char *[]);
 void ui_mainloop(int);
 
 int main(int argc, char *argv[]) {
-  // Initialize the monitor. 
+  // Initialize the monitor.
   int is_batch_mode = init_monitor(argc, argv);
-	//is_batch_mode=0;
-  // Receive commands from user. 
+  is_batch_mode     = 0;
+  // Receive commands from user.
   ui_mainloop(is_batch_mode);
 
   return 0;
@@ -16,53 +16,52 @@ int main(int argc, char *argv[]) {
 /* Code below is made for test */
 
 /*#include<stdio.h>
-#include<stdint.h>
-#include<stdbool.h>
 #include<assert.h>
+#include<stdbool.h>
+#include<stdint.h>
 #include<string.h>
 uint32_t expr(char *e, bool *success);
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
 
 int main(int argc, char *argv[]) {
-  // Initialize the monitor. 
+  // Initialize the monitor.
   int is_batch_mode = init_monitor(argc, argv);
-  
-	FILE * fp = fopen("./tools/gen-expr/input","r");
-	assert(fp!=NULL);
-	
-	int i,LOOP;
-	bool correct = 1;
-	uint32_t res1,res2;
-	char ex[1024];
-	fscanf(fp,"%d",&LOOP); //read LOOP times
-	for(i=1;i<=100;i++){			
-		fscanf(fp,"%u",&res1);//read correct answer
-		fgets(ex,1024,fp); //read expression
-		ex[strlen(ex)-1]='\0';
-		bool* tmp=0;
-					
-		FILE * fp2 = freopen("/tmp/nemu-log","w",stdout);
-		printf("%u\n",expr(ex,tmp)); //result is saved in nemu-log file
-		fclose(fp2);
-		
-		fp2 = fopen("/tmp/nemu-log","r"); //reread result from log file
-		fscanf(fp2,"%u",&res2);
-		fclose(fp2);
-		
-		if(res1!=res2){
-			fprintf(stderr,"Wrong result occured:Line %d Correct answer:%u Wrong answer:%u\n",i,res1,res2);
-			correct=0;
-		}
-	}
-	fclose(fp);
-	freopen("/dev/tty","w",stdout); // return the status of stdout
-	if(correct) printf("\n\nCongratulations!It seems that nothing wrong in your evaluation.\n\n");
 
-  // Receive commands from user. 
+  FILE * fp = fopen("./tools/gen-expr/input","r");
+  assert(fp!=NULL);
+
+  int i,LOOP;
+  bool correct = 1;
+  uint32_t res1,res2;
+  char ex[1024];
+  fscanf(fp,"%d",&LOOP); //read LOOP times
+  for(i=1;i<=100;i++){
+    fscanf(fp,"%u",&res1);//read correct answer
+    fgets(ex,1024,fp); //read expression
+    ex[strlen(ex)-1]='\0';
+    bool* tmp=0;
+
+    FILE * fp2 = freopen("/tmp/nemu-log","w",stdout);
+    printf("%u\n",expr(ex,tmp)); //result is saved in nemu-log file
+    fclose(fp2);
+
+    fp2 = fopen("/tmp/nemu-log","r"); //reread result from log file
+    fscanf(fp2,"%u",&res2);
+    fclose(fp2);
+
+    if(res1!=res2){
+      fprintf(stderr,"Wrong result occured:Line %d Correct answer:%u Wrong
+answer:%u\n",i,res1,res2); correct=0;
+    }
+  }
+  fclose(fp);
+  freopen("/dev/tty","w",stdout); // return the status of stdout
+  if(correct) printf("\n\nCongratulations!It seems that nothing wrong in your
+evaluation.\n\n");
+
+  // Receive commands from user.
   ui_mainloop(is_batch_mode);
 
   return 0;
 }*/
-
-
