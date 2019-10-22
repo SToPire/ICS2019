c8d260676d1869770dd56b17a15d571eae7d5a37
tracer-ics2019 <tracer@njuics.org>
2019-10-22 10:28:27 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-31-generic #33~18.04.1-Ubuntu SMP Tue Oct 1 10:20:39 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  10:28:27 up 23 min,  1 user,  load average: 0.64, 0.55, 0.49 8655291e2380ab395840bedbcb5ac2a6be879d74

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -105,15 +105,15 @@ make_EHelper(adc)
 
     rtl_update_ZFSF(&s1, id_dest->width);
 
+    // update OF
+    rtl_is_add_overflow(&s0, &s1, &id_dest->val, &id_src->val, id_dest->width);
+    rtl_set_OF(&s0);
+
     // update CF
     rtl_is_add_carry(&s1, &s1, &s0);
     rtl_is_add_carry(&s0, &s0, &id_dest->val);
     rtl_or(&s0, &s0, &s1);
     rtl_set_CF(&s0);
-
-    // update OF
-    rtl_is_add_overflow(&s0, &s1, &id_dest->val, &id_src->val, id_dest->width);
-    rtl_set_OF(&s0);
 
     print_asm_template2(adc);
 }
