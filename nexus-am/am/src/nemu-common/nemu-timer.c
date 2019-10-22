78556cbe74d9e2024e8a930bc8908096a5cda842
tracer-ics2019 <tracer@njuics.org>
2019-10-22 21:41:38 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-31-generic #33~18.04.1-Ubuntu SMP Tue Oct 1 10:20:39 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  21:41:38 up  3:07,  1 user,  load average: 0.23, 0.35, 0.51 603c30119d569ea85c85e9ed2ea3592fc6b9cfb2

--- nexus-am/am/src/nemu-common/nemu-timer.c
+++ nexus-am/am/src/nemu-common/nemu-timer.c
@@ -2,23 +2,26 @@
 #include <amdev.h>
 #include <nemu.h>
 
+static uint32_t boot_time;
+
 size_t __am_timer_read(uintptr_t reg, void* buf, size_t size)
 {
     switch (reg) {
         case _DEVREG_TIMER_UPTIME: {
+            uint32_t now = inl(RTC_ADDR);
             _DEV_TIMER_UPTIME_t* uptime = (_DEV_TIMER_UPTIME_t*)buf;
-            uptime->hi                  = 0;
-            uptime->lo                  = 0;
+            uptime->hi = 0;
+            uptime->lo = now - boot_time;
             return sizeof(_DEV_TIMER_UPTIME_t);
         }
         case _DEVREG_TIMER_DATE: {
             _DEV_TIMER_DATE_t* rtc = (_DEV_TIMER_DATE_t*)buf;
-            rtc->second            = 0;
-            rtc->minute            = 0;
-            rtc->hour              = 0;
-            rtc->day               = 0;
-            rtc->month             = 0;
-            rtc->year              = 2000;
+            rtc->second = 0;
+            rtc->minute = 0;
+            rtc->hour = 0;
+            rtc->day = 0;
+            rtc->month = 0;
+            rtc->year = 2000;
             return sizeof(_DEV_TIMER_DATE_t);
         }
     }
@@ -27,4 +30,5 @@ size_t __am_timer_read(uintptr_t reg, void* buf, size_t size)
 
 void __am_timer_init()
 {
+    boot_time = inl(RTC_ADDR);
 }
