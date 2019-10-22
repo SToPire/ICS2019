3cb841166681b0f82c894188a39b3029811aca64
tracer-ics2019 <tracer@njuics.org>
2019-10-22 21:13:02 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-31-generic #33~18.04.1-Ubuntu SMP Tue Oct 1 10:20:39 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  21:13:02 up  2:38,  1 user,  load average: 1.59, 1.20, 0.96 61f67ed4d4197d0bd04dfdcb1eae2b666c795c24

--- nexus-am/am/src/nemu-common/nemu-timer.c
+++ nexus-am/am/src/nemu-common/nemu-timer.c
@@ -4,7 +4,6 @@
 
 size_t __am_timer_read(uintptr_t reg, void* buf, size_t size)
 {
-    _putc('c');
     switch (reg) {
         case _DEVREG_TIMER_UPTIME: {
             _DEV_TIMER_UPTIME_t* uptime = (_DEV_TIMER_UPTIME_t*)buf;
