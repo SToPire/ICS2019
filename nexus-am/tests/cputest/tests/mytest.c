980ab795db43f1aba222103c34081d10c8e73404
tracer-ics2019 <tracer@njuics.org>
2019-10-23 16:18:19 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  16:18:19 up  2:23,  1 user,  load average: 1.75, 1.31, 0.80 76e8cac98e1d453f480cdf38ef7a0cf29dbe9bb5

--- nexus-am/tests/cputest/tests/mytest.c
+++ nexus-am/tests/cputest/tests/mytest.c
@@ -3,10 +3,8 @@
 int main()
 {
     char out[1024];
-    printf("%d-%d-%d %02d:%02d:%02d GMT (", 0, 0, 0, 0, 0, 0);
-    //int a = sprintf(out, "%s", "hello");
-    //int a = sprintf(out, "hello");
-    //printf("%d\n", a);
+    // printf("%d-%d-%d %02d:%02d:%02d GMT (", 0, 0, 0, 0, 0, 0);
+    printf("%d", 0);
     nemu_assert(sprintf(out, "%4d", -12345) == 6);
     nemu_assert(strcmp(out, "-12345") == 0);
 }
