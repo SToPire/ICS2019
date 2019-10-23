3da4c69ff8e6638cf3a15f8f82106862d0a7cbb5
tracer-ics2019 <tracer@njuics.org>
2019-10-23 16:02:55 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  16:02:55 up  2:07,  1 user,  load average: 0.70, 0.45, 0.34 58cc32c5c3a3d87d4c277f317913a0aacfc7b74

--- nexus-am/tests/cputest/tests/mytest.c
+++ nexus-am/tests/cputest/tests/mytest.c
@@ -3,7 +3,7 @@
 int main()
 {
     char out[1024];
-    printf("%9d", -12345);
+    printf("%9d", 12345);
     //int a = sprintf(out, "%s", "hello");
     //int a = sprintf(out, "hello");
     //printf("%d\n", a);
