7c705c2a6d8b404b14b41b3a1928e68d5d4dc99c
tracer-ics2019 <tracer@njuics.org>
2019-10-23 13:25:27 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  13:25:27 up  1:18,  1 user,  load average: 0.15, 0.38, 0.35 9cc41d53d1096c40c54b2938831ff6067607bcc5

--- nexus-am/tests/cputest/tests/mytest.c
+++ nexus-am/tests/cputest/tests/mytest.c
@@ -4,6 +4,6 @@ int main()
 {
     char out[1024];
     printf("%s", "hello");
-    nemu_assert(sprintf(out, "hello") == 6);
+    nemu_assert(sprintf(out, "%s", "hello") == 5);
     //nemu_assert(strcmp(out, "hello") == 0);
 }
