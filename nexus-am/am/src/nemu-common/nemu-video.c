f1f2656bd5c5fb4bee68e0bc3571ba28ebc75eea
tracer-ics2019 <tracer@njuics.org>
2019-10-24 14:18:09 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  14:18:09 up  4:48,  1 user,  load average: 1.04, 0.91, 0.70 5997adb55823253cc37b8c739e3811ea78ebb18b

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -27,10 +27,9 @@ size_t __am_video_write(uintptr_t reg, void* buf, size_t size)
             int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
             uint32_t* pixels = ctl->pixels;
             int cp_bytes = w < 400 - x ? w : 400 - x;
-            for (int j = 0; j < h && y + j < 300; j++) {
+            for (int j = 0; j < h && y + j < 300; j++)
                 for (int i = 0; i < cp_bytes; i++)
                     fb[(y + j) * 400 + x + i] = *pixels++;
-            }
             if (ctl->sync) {
                 outl(SYNC_ADDR, 0);
             }
