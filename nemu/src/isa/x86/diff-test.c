bbde1fb6bd3849e0db394dd79f5e1e3ae02e6498
tracer-ics2019 <tracer@njuics.org>
2019-10-24 16:11:13 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  16:11:13 up 8 min,  1 user,  load average: 1.58, 1.02, 0.53 bcf6fc6b49b258a9f5ee298b814cb44a5fe443d7

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -6,15 +6,15 @@ bool isa_difftest_checkregs(CPU_state* ref_r, vaddr_t pc)
     uint32_t r[77];
     ref_difftest_getregs(r);
     bool ans = true;
-    if (r[0] != ref_r->eax) ans = false;
-    if (r[1] != ref_r->ecx) ans = false;
-    if (r[2] != ref_r->edx) ans = false;
-    if (r[3] != ref_r->ebx) ans = false;
-    if (r[4] != ref_r->esp) ans = false;
-    if (r[5] != ref_r->ebp) ans = false;
-    if (r[6] != ref_r->esi) ans = false;
-    if (r[7] != ref_r->edi) ans = false;
-    if (r[8] != ref_r->pc) ans = false;
+    if (cpu.eax != ref_r->eax) ans = false;
+    if (cpu.ecx != ref_r->ecx) ans = false;
+    if (cpu.edx != ref_r->edx) ans = false;
+    if (cpu.ebx != ref_r->ebx) ans = false;
+    if (cpu.esp != ref_r->esp) ans = false;
+    if (cpu.ebp != ref_r->ebp) ans = false;
+    if (cpu.esi != ref_r->esi) ans = false;
+    if (cpu.edi != ref_r->edi) ans = false;
+    if (cpu.pc != ref_r->pc) ans = false;
     return ans;
 }
 
