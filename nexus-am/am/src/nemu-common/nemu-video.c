a9458196264a5be95f67feae81f54a4c1474f201
tracer-ics2019 <tracer@njuics.org>
2019-10-24 11:23:16 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  11:23:16 up  1:53,  1 user,  load average: 1.20, 1.07, 1.01 ce454f6c7578ac69ce309b014a1fa71e62cd4f34

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -1,6 +1,9 @@
 #include <am.h>
 #include <amdev.h>
 #include <nemu.h>
+int screen_width();
+int screen_height();
+void draw_sync();
 
 size_t __am_video_read(uintptr_t reg, void* buf, size_t size)
 {
@@ -38,9 +41,9 @@ size_t __am_video_write(uintptr_t reg, void* buf, size_t size)
 
 void __am_vga_init()
 {
-    // int i;
-    // int size = screen_width() * screen_height();
-    // uint32_t* fb = (uint32_t*)(uintptr_t)FB_ADDR;
-    // for (i = 0; i < size; i++) fb[i] = i;
-    // draw_sync();
+    int i;
+    int size = screen_width() * screen_height();
+    uint32_t* fb = (uint32_t*)(uintptr_t)FB_ADDR;
+    for (i = 0; i < size; i++) fb[i] = i;
+    draw_sync();
 }
