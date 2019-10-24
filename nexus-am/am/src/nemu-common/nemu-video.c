9d3c540f79ca2a5899acaff2abebb8e6d2c12081
tracer-ics2019 <tracer@njuics.org>
2019-10-24 12:50:29 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  12:50:29 up  3:20,  1 user,  load average: 0.49, 1.02, 1.11 9a8e5065755aa7cba1c9e6d8fe30ab294d40fee

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -10,8 +10,8 @@ size_t __am_video_read(uintptr_t reg, void* buf, size_t size)
     switch (reg) {
         case _DEVREG_VIDEO_INFO: {
             _DEV_VIDEO_INFO_t* info = (_DEV_VIDEO_INFO_t*)buf;
-            info->width = 800;
-            info->height = 640;
+            info->width = 450;
+            info->height = 370;
             return sizeof(_DEV_VIDEO_INFO_t);
         }
     }
@@ -26,10 +26,10 @@ size_t __am_video_write(uintptr_t reg, void* buf, size_t size)
             uint32_t* fb = (uint32_t*)(uintptr_t)FB_ADDR;
             int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
             uint32_t* pixels = ctl->pixels;
-            int cp_bytes = sizeof(uint32_t) * (w < 800 - x ? w : 800 - x);
-            for (int j = 0; j < h && y + j < 640; j++) {
+            int cp_bytes = sizeof(uint32_t) * (w < 450 - x ? w : 450 - x);
+            for (int j = 0; j < h && y + j < 370; j++) {
                 for (int i = 0; i < cp_bytes; i++)
-                    fb[(y + j) * 800 + x + i] = *pixels;
+                    fb[(y + j) * 450 + x + i] = *pixels;
                 pixels += w;
             }
             if (ctl->sync) {
