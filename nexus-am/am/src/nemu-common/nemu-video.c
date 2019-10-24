1c5ec26fda5616188041b284941d633d784ee086
tracer-ics2019 <tracer@njuics.org>
2019-10-24 11:52:34 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  11:52:34 up  2:23,  1 user,  load average: 1.27, 1.09, 1.03 3cbc08fd5b8488c11244b4cc21b784b6acfdb31f

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -10,7 +10,7 @@ size_t __am_video_read(uintptr_t reg, void* buf, size_t size)
     switch (reg) {
         case _DEVREG_VIDEO_INFO: {
             _DEV_VIDEO_INFO_t* info = (_DEV_VIDEO_INFO_t*)buf;
-            info->width = inl(SCREEN_ADDR);
+            info->width = 400;
             info->height = 320;
             return sizeof(_DEV_VIDEO_INFO_t);
         }
