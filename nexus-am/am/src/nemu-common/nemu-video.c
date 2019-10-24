3ab4f57d30f911ac0199ec1dcd8c16bf46c6d6e3
tracer-ics2019 <tracer@njuics.org>
2019-10-24 11:39:28 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  11:39:28 up  2:09,  1 user,  load average: 0.93, 0.96, 0.99 c48979d447db9b8c7b4f2c92fc635a8d1ecd3479

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -23,13 +23,15 @@ size_t __am_video_write(uintptr_t reg, void* buf, size_t size)
     switch (reg) {
         case _DEVREG_VIDEO_FBCTL: {
             _DEV_VIDEO_FBCTL_t* ctl = (_DEV_VIDEO_FBCTL_t*)buf;
-            // int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-            // uint32_t* pixels = ctl->pixels;
-            // int cp_bytes = sizeof(uint32_t) * min(w, 400 - x);
-            // for (int j = 0; j < h && y + j < 320;j++){
-            //     memcpy(&fb[(y + j) * 400 + x], pixels, cp_bytes);
-            //     pixels += w;
-            // }
+            uint32_t* fb = (uint32_t*)(uintptr_t)FB_ADDR;
+            int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+            uint32_t* pixels = ctl->pixels;
+            int cp_bytes = sizeof(uint32_t) * (w < 400 - x ? w : 400 - x);
+            for (int j = 0; j < h && y + j < 320; j++) {
+                // memcpy(&fb[(y + j) * 400 + x], pixels, cp_bytes);
+                for (int i = 0; i < cp_bytes; i++)
+                    fb[(y + j) * 400 + x + i] = *pixels;
+            }
             if (ctl->sync) {
                 outl(SYNC_ADDR, 0);
             }
