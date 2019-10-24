6eccc19d983a20dff362718383a91eade87ea870
tracer-ics2019 <tracer@njuics.org>
2019-10-24 21:15:32 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  21:15:32 up 52 min,  1 user,  load average: 0.55, 0.55, 0.38 1765aba235477ec978f3853f6a3f0832bd393e6

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -81,7 +81,7 @@ static inline void interpret_rtl_idiv64_r(rtlreg_t* dest, const rtlreg_t* src1_h
     *dest = dividend % divisor;
 }
 
-static void interpret_rtl_lm(rtlreg_t* dest, const rtlreg_t* addr, int len)
+static inline void interpret_rtl_lm(rtlreg_t* dest, const rtlreg_t* addr, int len)
 {
     *dest = vaddr_read(*addr, len);
 }
