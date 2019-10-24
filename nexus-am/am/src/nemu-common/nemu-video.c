80748a133edefd8bf578d5fa96c3e9ea737be21d
tracer-ics2019 <tracer@njuics.org>
2019-10-24 13:06:50 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  13:06:50 up  3:37,  1 user,  load average: 0.87, 0.78, 0.93 5cd2b6d64f99ae54b74d4781a8e3ddc0ecbbad8f

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -11,7 +11,7 @@ size_t __am_video_read(uintptr_t reg, void* buf, size_t size)
         case _DEVREG_VIDEO_INFO: {
             _DEV_VIDEO_INFO_t* info = (_DEV_VIDEO_INFO_t*)buf;
             info->width = 400;
-            info->height = 300;
+            info->height = 320;
             return sizeof(_DEV_VIDEO_INFO_t);
         }
     }
@@ -27,7 +27,7 @@ size_t __am_video_write(uintptr_t reg, void* buf, size_t size)
             int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
             uint32_t* pixels = ctl->pixels;
             int cp_bytes = sizeof(uint32_t) * (w < 400 - x ? w : 400 - x);
-            for (int j = 0; j < h && y + j < 300; j++) {
+            for (int j = 0; j < h && y + j < 320; j++) {
                 for (int i = 0; i < cp_bytes; i++)
                     fb[(y + j) * 400 + x + i] = *pixels;
                 pixels += w;
