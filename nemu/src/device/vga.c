8fbe0b75c0f286773d413d380e160f1ec58db72e
tracer-ics2019 <tracer@njuics.org>
2019-10-24 10:54:32 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  10:54:32 up  1:25,  1 user,  load average: 1.37, 1.19, 1.11 8d9ecaedd167b34ca1b53edb7a2e565fe88b07e

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -32,7 +32,7 @@ static inline void update_screen()
 static void vga_io_handler(uint32_t offset, int len, bool is_write)
 {
     // TODO: call `update_screen()` when writing to the sync register
-    assert(offset == 0);
+    printf("offset:%x len:%d is_write:%d\n", offset, len, is_write);
 }
 
 void init_vga()
