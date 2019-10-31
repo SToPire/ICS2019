04761adeb521c2460db5629dcbfb7029e9bd1cbd
tracer-ics2019 <tracer@njuics.org>
2019-10-31 17:35:56 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:35:56 up  3:33,  1 user,  load average: 1.13, 2.26, 2.30 f95888dc5a312effd7ccdcd458ebedaff9866cd3

--- nexus-am/am/src/x86/nemu/cte.c
+++ nexus-am/am/src/x86/nemu/cte.c
@@ -40,7 +40,7 @@ int _cte_init(_Context* (*handler)(_Event, _Context*))
     // ---------------------- system call ----------------------------
     idt[0x80] = GATE(STS_TG32, KSEL(SEG_KCODE), __am_vecsys, DPL_USER);
     idt[0x81] = GATE(STS_TG32, KSEL(SEG_KCODE), __am_vectrap, DPL_KERN);
-    _putc('c');
+
     set_idt(idt, sizeof(idt));
 
     // register event handler
