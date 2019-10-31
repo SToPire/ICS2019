eb11d5a2070575aaee980963d8cbdf1a3beb77fc
tracer-ics2019 <tracer@njuics.org>
2019-10-31 21:54:54 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  21:54:54 up  7:52,  1 user,  load average: 1.04, 0.68, 0.47 2f60939195522cd495ecfa390513f0e25079308

--- nemu/src/isa/x86/exec/system.c
+++ nemu/src/isa/x86/exec/system.c
@@ -25,7 +25,7 @@ make_EHelper(mov_cr2r)
 make_EHelper(int)
 {
     rtl_mv(&s0, &cpu.IDTR);
-    printf("%x\n", vaddr_read(s0, 1));
+    printf("%x\n", vaddr_read(s0 + 3, 1));
     print_asm("int %s", id_dest->str);
 
     difftest_skip_dut(1, 2);
