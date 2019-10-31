eba1839cc52125344bec08d83d0f74ecc1936875
tracer-ics2019 <tracer@njuics.org>
2019-10-31 16:58:38 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  16:58:38 up  2:55,  1 user,  load average: 1.02, 0.99, 0.91 fba5eaabafe7102212d65e95747d8d5c850cc11c

--- nemu/src/isa/x86/include/isa/reg.h
+++ nemu/src/isa/x86/include/isa/reg.h
@@ -6,9 +6,30 @@
 
 #define PC_START IMAGE_START
 
-enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
-enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
-enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
+enum { R_EAX,
+       R_ECX,
+       R_EDX,
+       R_EBX,
+       R_ESP,
+       R_EBP,
+       R_ESI,
+       R_EDI };
+enum { R_AX,
+       R_CX,
+       R_DX,
+       R_BX,
+       R_SP,
+       R_BP,
+       R_SI,
+       R_DI };
+enum { R_AL,
+       R_CL,
+       R_DL,
+       R_BL,
+       R_AH,
+       R_CH,
+       R_DH,
+       R_BH };
 
 /* TODO: Re-organize the `CPU_state' structure to match the register
  * encoding scheme in i386 instruction format. For example, if we
@@ -17,57 +38,69 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * For more details about the register encoding scheme, see i386 manual.
  */
 
-typedef struct {
-	union{
-		struct {
-			rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-		};
-		union {
-			uint32_t _32;
-			uint16_t _16;
-			uint8_t _8[2];
-		} gpr[8];
-  	};
+typedef struct
+{
+    union
+    {
+        struct
+        {
+            rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+        };
+        union
+        {
+            uint32_t _32;
+            uint16_t _16;
+            uint8_t _8[2];
+        } gpr[8];
+    };
 
-  /* Do NOT change the order of the GPRs' definitions. */
+    /* Do NOT change the order of the GPRs' definitions. */
 
-  /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
+    /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
    * in PA2 able to directly access these registers.
    */
-  
-  vaddr_t pc;
-	
-  struct {
-  	rtlreg_t CF:1;
-  	rtlreg_t ZF:1;
-  	rtlreg_t SF:1;
-  	rtlreg_t IF:1;
-  	rtlreg_t OF:1;
-  }eflags;
-		
+
+    vaddr_t pc;
+    union
+    {
+        struct
+        {
+            rtlreg_t EMPTY : 3;
+            rtlreg_t CF : 1;
+            rtlreg_t ZF : 1;
+            rtlreg_t SF : 1;
+            rtlreg_t IF : 1;
+            rtlreg_t OF : 1;
+        } eflags;
+        uint8_t EFLAGS;
+    };
+    uint32_t cs;
+
 } CPU_state;
 
-static inline int check_reg_index(int index) {
-  assert(index >= 0 && index < 8);
-  return index;
+static inline int check_reg_index(int index)
+{
+    assert(index >= 0 && index < 8);
+    return index;
 }
 
 #define reg_l(index) (cpu.gpr[check_reg_index(index)]._32)
 #define reg_w(index) (cpu.gpr[check_reg_index(index)]._16)
 #define reg_b(index) (cpu.gpr[check_reg_index(index) & 0x3]._8[index >> 2])
 
-static inline const char* reg_name(int index, int width) {
-  extern const char* regsl[];
-  extern const char* regsw[];
-  extern const char* regsb[];
-  assert(index >= 0 && index < 8);
+static inline const char* reg_name(int index, int width)
+{
+    extern const char* regsl[];
+    extern const char* regsw[];
+    extern const char* regsb[];
+    assert(index >= 0 && index < 8);
 
-  switch (width) {
-    case 4: return regsl[index];
-    case 1: return regsb[index];
-    case 2: return regsw[index];
-    default: assert(0);
-  }
+    switch (width) {
+        case 4: return regsl[index];
+        case 1: return regsb[index];
+        case 2: return regsw[index];
+        default: assert(0);
+    }
 }
 
 #endif
