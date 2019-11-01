a2dd69cacbad0a907e755af0464df1be2ef7c26f
tracer-ics2019 <tracer@njuics.org>
2019-11-01 13:53:56 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  13:53:56 up  2:06,  1 user,  load average: 2.93, 4.60, 3.05 7844084d45494a04286e6f01d2c3c7d1a4bcfb4

--- nemu/src/isa/x86/exec/system.c
+++ nemu/src/isa/x86/exec/system.c
@@ -36,7 +36,7 @@ make_EHelper(iret)
     rtl_pop(&cpu.pc);
     rtl_pop(&cpu.cs);
     rtl_pop(&cpu.EFLAGS);
-    printf("eflags:%x\n", cpu.EFLAGS);
+    printf("eflags:%x\n", cpu.cs);
 
     print_asm("iret");
 }
