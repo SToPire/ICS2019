4c6a69ff12a0b36d88e8f1121292693cfcfe38b9
tracer-ics2019 <tracer@njuics.org>
2019-11-01 13:35:29 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  13:35:29 up  1:48,  1 user,  load average: 1.26, 1.05, 1.81 45fc2c47f8de40d25e945b3e1211f5dcd2e3691f

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@ int printf(const char* fmt, ...);
 static _Context* do_event(_Event e, _Context* c)
 {
     switch (e.event) {
-        case _EVENT_YIELD: printf("Hey man!\n");
+        case _EVENT_YIELD: printf("Hey man!\n"); break;
         default: panic("Unhandled event ID = %d", e.event);
     }
 
