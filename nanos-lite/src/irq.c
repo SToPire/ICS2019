b6b748a265f24e177cc6199ee36127e9a32ec843
tracer-ics2019 <tracer@njuics.org>
2019-11-13 19:23:12 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:23:12 up  5:56,  1 user,  load average: 0.68, 0.66, 0.62 95df6e56a365d2ddc755fa3e837a6ecd64ecac60

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,9 +1,10 @@
 #include "common.h"
 int printf(const char* fmt, ...);
+_Context* do_syscall(_Context* c);
 static _Context* do_event(_Event e, _Context* c)
 {
     switch (e.event) {
-        case _EVENT_SYSCALL: printf("Hey SYSCALL!\n"); break;
+        case _EVENT_SYSCALL: do_syscall(c); break;
         case _EVENT_YIELD: printf("Hey YIELD!\n"); break;
         default: panic("Unhandled event ID = %d", e.event);
     }
