3fbdc8444b08e042cb2990ec2a10c2265beac8e3
tracer-ics2019 <tracer@njuics.org>
2019-11-13 19:41:21 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:41:21 up  6:14,  1 user,  load average: 0.41, 0.66, 0.65 545461d2f887722ae3035949bd84f793429f7c99

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,13 +1,19 @@
 #include "common.h"
 #include "syscall.h"
 
+void sys_yield(_Context* c)
+{
+    _yield();
+    c->GPRx = 0;
+}
+
 _Context* do_syscall(_Context* c)
 {
     uintptr_t a[4];
     a[0] = c->GPR1;
 
     switch (a[0]) {
-        case SYS_yield: _yield(); return 0;
+        case SYS_yield: sys_yield(c); break;
         default: panic("Unhandled syscall ID = %d", a[0]);
     }
 
