d8e61a369faf6886b22f03fc8352b336a174857f
tracer-ics2019 <tracer@njuics.org>
2019-11-13 17:58:07 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:58:07 up  4:31,  1 user,  load average: 0.71, 0.55, 0.53 2255c20118656da79a47005443d2908ac582df50

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,7 +16,7 @@ static uintptr_t loader(PCB* pcb, const char* filename)
     Elf_Ehdr E_hdr;
     Elf_Phdr P_hdr;
     ramdisk_read(&E_hdr, 0x0, sizeof(Elf_Ehdr));
-    printf("%x\n", E_hdr.e_entry);
+    printf("%x\n", E_hdr.e_ident);
     for (int i = 0; i < E_hdr.e_phnum; i++) {
         ramdisk_read(&P_hdr, E_hdr.e_phoff + i * E_hdr.e_phentsize, E_hdr.e_phentsize);
         if (P_hdr.p_type == PT_LOAD) {
@@ -26,7 +26,7 @@ static uintptr_t loader(PCB* pcb, const char* filename)
             memset((void*)P_hdr.p_filesz, 0, P_hdr.p_memsz - P_hdr.p_filesz);
         }
     }
-    return 0x30000f0;
+    return E_hdr.e_entry;
 }
 
 void naive_uload(PCB* pcb, const char* filename)
