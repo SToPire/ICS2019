6e479c34bb49bb38d6314d5018148b0853deb427
tracer-ics2019 <tracer@njuics.org>
2019-11-13 20:28:56 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:28:56 up  7:02,  1 user,  load average: 1.27, 1.16, 0.88 a360699d10de7048895b8e43ddda981a67367852

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -9,7 +9,7 @@ int main()
     while (1) {
         j++;
         if (j == 10000) {
-            printf("Hello World from Navy-apps for the %dth time!\n", 1);
+            printf("Hello World from Navy-apps for the %dth time!\n", i++);
             j = 0;
         }
     }
