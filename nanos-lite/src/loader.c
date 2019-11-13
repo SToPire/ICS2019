e0695726ef9d0c4a9332adf959dc6d18e7f82994
tracer-ics2019 <tracer@njuics.org>
2019-11-13 17:13:19 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:13:19 up  3:46,  1 user,  load average: 1.21, 0.55, 0.44 55bdf0086a5de05b69c3843884d3eada6e4b5117

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -23,7 +23,7 @@ static uintptr_t loader(PCB* pcb, const char* filename)
         }
         printf("%d:%x\n", i, P_hdr.p_type);
     }
-    return 0x3000000;
+    return 0;
 }
 
 void naive_uload(PCB* pcb, const char* filename)
