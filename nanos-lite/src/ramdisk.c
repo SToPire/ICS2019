4d5654971522e061f8db5ef857ef58f0d5afc18f
tracer-ics2019 <tracer@njuics.org>
2019-11-13 10:59:22 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  10:59:22 up 55 min,  1 user,  load average: 1.06, 0.89, 0.77 7f2bc0428f81a4378006a48323e27cb1bdf33d01

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -29,6 +29,7 @@ void init_ramdisk()
 {
     Log("ramdisk info: start = %p, end = %p, size = %d bytes",
         &ramdisk_start, &ramdisk_end, RAMDISK_SIZE);
+    Log("fuck you : %x\n fuck you: %u", ramdisk_start, ramdisk_start);
 }
 
 size_t get_ramdisk_size()
