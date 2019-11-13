22b59dd4ec04e85c9c3209e868edaaeb36efb8b3
tracer-ics2019 <tracer@njuics.org>
2019-11-13 19:34:06 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:34:06 up  6:07,  1 user,  load average: 0.95, 0.75, 0.66 f3517dc01e8be3ea22d5eb2c5dfdbee17890f74d

--- nexus-am/am/include/arch/x86-nemu.h
+++ nexus-am/am/include/arch/x86-nemu.h
@@ -1,8 +1,7 @@
 #ifndef __ARCH_H__
 #define __ARCH_H__
 
-struct _Context
-{
+struct _Context {
     struct _AddressSpace* as;
     //uintptr_t esi, ebx, eax, eip, edx, eflags, ecx, cs, esp, edi, ebp;
     uintptr_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
@@ -11,9 +10,9 @@ struct _Context
 };
 
 #define GPR1 eax
-#define GPR2 eip
-#define GPR3 eip
-#define GPR4 eip
-#define GPRx eip
+#define GPR2 ebx
+#define GPR3 ecx
+#define GPR4 edx
+#define GPRx eax
 
 #endif
