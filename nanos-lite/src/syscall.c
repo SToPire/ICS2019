ce46fdd8f66b6799c9fcdf9187eda7ab68219c22
tracer-ics2019 <tracer@njuics.org>
2019-11-13 20:19:34 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:19:34 up  6:52,  1 user,  load average: 1.19, 0.98, 0.63 3c838df97c1342b25d31894391dce4b5ab651f76

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,23 +1,31 @@
 #include "common.h"
 #include "syscall.h"
 
-void sys_yield(_Context* c)
+int sys_yield()
 {
     _yield();
-    c->GPRx = 0;
+    return 0;
 }
-void sys_exit(_Context* c)
+int sys_exit(uintptr_t arg)
 {
-    _halt(c->GPR2);
+    _halt(arg);
+    return 0;
+}
+int sys_write(_Context* c)
+{
+    return 0;
 }
 _Context* do_syscall(_Context* c)
 {
     uintptr_t a[4];
     a[0] = c->GPR1;
-
+    a[1] = c->GPR2;
+    a[2] = c->GPR3;
+    a[3] = c->GPR4;
     switch (a[0]) {
-        case SYS_yield: sys_yield(c); break;
-        case SYS_exit: sys_exit(c); break;
+        case SYS_write: c->GPRx = sys_write(c); break;
+        case SYS_yield: c->GPRx = sys_yield(); break;
+        case SYS_exit: c->GPRx = sys_exit(a[1]); break;
         default: panic("Unhandled syscall ID = %d", a[0]);
     }
 
