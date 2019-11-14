bea757f75b86c937b0709c42426a92906f2f25d1
tracer-ics2019 <tracer@njuics.org>
2019-11-14 10:25:29 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  10:25:29 up  2:22,  1 user,  load average: 1.91, 1.08, 0.79 e0567ad7a694ce7a808fa3af41b1be0caf2f7f9e

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -13,9 +13,8 @@ int sys_exit(int status)
 }
 int sys_write(int fd, void* buf, size_t count)
 {
-    int i = 0;
     if (fd == 1 || fd == 2) {
-        for (; i < count; i++)
+        for (int i = 0; i < count; i++)
             _putc(*(char*)(buf + i));
         return count;
     } else
