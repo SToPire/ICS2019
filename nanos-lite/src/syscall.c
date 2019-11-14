679decebd481de2a9b8af5672d662c1214e9bfce
tracer-ics2019 <tracer@njuics.org>
2019-11-14 10:47:08 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  10:47:08 up  2:43,  1 user,  load average: 1.05, 1.05, 0.97 a980fb9d3a70c9cd71756330b8bcaf41e6c3b823

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -13,13 +13,21 @@ int sys_exit(int status)
 }
 int sys_write(int fd, void* buf, size_t count)
 {
-    Log("fd:%d buf:%p count: %u\n", fd, buf, count);
+    //Log("fd:%d buf:%p count: %u\n", fd, buf, count);
     if (fd == 1 || fd == 2) {
         for (int i = 0; i < count; i++)
             _putc(*(char*)(buf + i));
         return count;
     } else
         return 0;
+}
+int sys_brk(intptr_t increment)
+{
+    char buf[20];
+    extern char end;
+    sprintf(buf, "%p", &end);
+    sys_write(1, buf, 10);
+    return 0;
 }
 _Context* do_syscall(_Context* c)
 {
@@ -29,9 +37,10 @@ _Context* do_syscall(_Context* c)
     a[2] = c->GPR3;
     a[3] = c->GPR4;
     switch (a[0]) {
-        case SYS_write: c->GPRx = sys_write((int)a[1], (void*)a[2], (size_t)a[3]); break;
-        case SYS_yield: c->GPRx = sys_yield(); break;
         case SYS_exit: c->GPRx = sys_exit((int)a[1]); break;
+        case SYS_yield: c->GPRx = sys_yield(); break;
+        case SYS_write: c->GPRx = sys_write((int)a[1], (void*)a[2], (size_t)a[3]); break;
+        case SYS_brk: c->GPRx = sys_brk((intptr_t)a[1]); break;
         default: panic("Unhandled syscall ID = %d", a[0]);
     }
     return NULL;
