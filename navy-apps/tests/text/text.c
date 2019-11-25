563ff1d68ed5126116fe1ca0ad34e7e2ca47113d
tracer-ics2019 <tracer@njuics.org>
2019-11-25 14:23:18 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  14:23:18 up  3:02,  1 user,  load average: 0.47, 0.74, 0.58 ef26d820aae87864f0af85fbecec0799c409367

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -3,8 +3,9 @@
 
 int main()
 {
-    //FILE* fp = fopen("/share/texts/num", "r+");
-    //assert(fp);
+    FILE* fp = fopen("/share/texts/num", "r+");
+    assert(fp);
+    printf("%x\n", fp);
 
     // fseek(fp, 0, SEEK_END);
     // long size = ftell(fp);
