b414254b37676a0774669e997c80a9423cd25cd1
tracer-ics2019 <tracer@njuics.org>
2019-11-25 15:39:21 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  15:39:21 up 25 min,  1 user,  load average: 0.67, 0.53, 0.48 c8de679153f650fcff65927dfff3f1eb8b16717

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -1,11 +1,9 @@
 #include <stdint.h>
-#include <assert.h>
 
 #define SYS_yield 1
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main()
 {
-    assert(1);
     return _syscall_(SYS_yield, 0, 0, 0);
 }
