5b9942d347b347988ea98e1b4e8af5eb15d0c797
tracer-ics2019 <tracer@njuics.org>
2019-11-26 02:19:36 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  02:19:36 up 11:05,  1 user,  load average: 1.11, 0.82, 0.78 8ca2d3dd1ef412094201f4921cef1a036aee890

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -80,7 +80,10 @@ __ssize_t fs_write(int fd, const void* buf, size_t len)
 {
     Finfo* cur_file = &file_table[fd];
     if (len > cur_file->size - cur_file->open_offset) len = cur_file->size - cur_file->open_offset;
-    ramdisk_write(buf, cur_file->disk_offset + cur_file->open_offset, len);
+    if (!cur_file->write)
+        ramdisk_write(buf, cur_file->disk_offset + cur_file->open_offset, len);
+    else
+        cur_file->write(buf, cur_file->disk_offset + cur_file->open_offset, len);
     cur_file->open_offset += len;
     return len;
 }
