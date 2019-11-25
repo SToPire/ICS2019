d3dcd1ae4a87742083aca70fb18e60a1075982ba
tracer-ics2019 <tracer@njuics.org>
2019-11-26 01:17:05 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  01:17:05 up 10:03,  1 user,  load average: 0.92, 0.61, 0.50 a1ff1b946cac991fae5df5c98db0d16ebb8f9738

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,6 +2,9 @@
 #include "syscall.h"
 int fs_open(const char* pathname, int flags, int mode);
 int fs_close(int fd);
+__ssize_t fs_read(int fd, void* buf, size_t len);
+__off_t fs_lseek(int fd, size_t offset, int whence);
+
 int sys_yield()
 {
     _yield();
@@ -34,6 +37,14 @@ int sys_close(int fd)
 {
     return fs_close(fd);
 }
+__ssize_t sys_read(int fd, void* buf, size_t count)
+{
+    return fs_read(fd, buf, count);
+}
+__off_t sys_lseek(int fd, __off_t offset, int whence)
+{
+    return fs_lseek(fd, offset, whence);
+}
 _Context* do_syscall(_Context* c)
 {
     uintptr_t a[4];
@@ -48,6 +59,8 @@ _Context* do_syscall(_Context* c)
         case SYS_brk: c->GPRx = sys_brk((intptr_t)a[1]); break;
         case SYS_open: c->GPRx = sys_open((const char*)a[1], (int)a[2], (__mode_t)a[3]); break;
         case SYS_close: c->GPRx = sys_close((int)a[1]); break;
+        case SYS_read: c->GPRx = sys_read((int)a[1], (void*)a[2], (size_t)a[3]); break;
+        case SYS_lseek: c->GPRx = sys_lseek((int)a[1], (__off_t)a[2], (int)a[3]); break;
         default: panic("Unhandled syscall ID = %d", a[0]);
     }
     return NULL;
