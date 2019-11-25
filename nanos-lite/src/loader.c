c12fbdb7c45b8f93a1acfa725099c627b3667537
tracer-ics2019 <tracer@njuics.org>
2019-11-25 13:18:36 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  13:18:36 up  1:57,  1 user,  load average: 0.27, 0.13, 0.10 bd1bc32cfe43f486d09ac9e1cc1b788f8253d418

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -13,7 +13,7 @@ size_t get_ramdisk_size();
 static uintptr_t loader(PCB* pcb, const char* filename)
 {
     char tmp[1005];
-    ramdisk_read(tmp, 0, 1000);
+    ramdisk_read(tmp, 572462, 1000);
     printf("%s\n", tmp);
     return 0;
     // Elf_Ehdr E_hdr;
