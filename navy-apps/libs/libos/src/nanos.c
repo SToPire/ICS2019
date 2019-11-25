f64a2d61603cca50e9b391c20f706fe41ffaf3e4
tracer-ics2019 <tracer@njuics.org>
2019-11-25 15:18:33 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  15:18:33 up 4 min,  1 user,  load average: 1.11, 0.84, 0.38 3abd8cc958d5cfdc8c07f1a458dfa44c90fc874

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -73,14 +73,13 @@ int _write(int fd, void* buf, size_t count)
 
 void* _sbrk(intptr_t increment)
 {
-    // if (_syscall_(SYS_brk, program_break + increment, 0, 0) == -1) {
-    //     return (void*)(-1);
-    // } else {
-    //     intptr_t tmp = program_break;
-    //     program_break += increment;
-    //     return (void*)tmp;
-    // }
-    return (void*)(-1);
+    if (_syscall_(SYS_brk, program_break + increment, 0, 0) == -1) {
+        return (void*)(-1);
+    } else {
+        intptr_t tmp = program_break;
+        program_break += increment;
+        return (void*)tmp;
+    }
 }
 
 int _read(int fd, void* buf, size_t count)
