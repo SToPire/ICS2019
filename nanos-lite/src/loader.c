efe48810348cf2fb6e13b7e02eeca59814c9a356
tracer-ics2019 <tracer@njuics.org>
2019-11-25 13:41:38 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  13:41:38 up  2:20,  1 user,  load average: 0.51, 0.35, 0.26 8852281a364a9746c8b4e93b5f891167390250d5

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -12,23 +12,19 @@ size_t ramdisk_read(void* buf, size_t offset, size_t len);
 size_t get_ramdisk_size();
 static uintptr_t loader(PCB* pcb, const char* filename)
 {
-    char tmp[1005];
-    ramdisk_read(tmp, 572462, 1000);
-    printf("%s\n", tmp);
-    return 0;
-    // Elf_Ehdr E_hdr;
-    // Elf_Phdr P_hdr;
-    // ramdisk_read(&E_hdr, 0x0, sizeof(Elf_Ehdr));
-    // for (int i = 0; i < E_hdr.e_phnum; i++) {
-    //     ramdisk_read(&P_hdr, E_hdr.e_phoff + i * E_hdr.e_phentsize, E_hdr.e_phentsize);
-    //     if (P_hdr.p_type == PT_LOAD) {
-    //         uint32_t tmp[get_ramdisk_size()];
-    //         ramdisk_read(tmp, P_hdr.p_offset, P_hdr.p_filesz);
-    //         memcpy((void*)P_hdr.p_vaddr, tmp, P_hdr.p_filesz);
-    //         memset((void*)(P_hdr.p_vaddr + P_hdr.p_filesz), 0, P_hdr.p_memsz - P_hdr.p_filesz);
-    //     }
-    // }
-    // return E_hdr.e_entry;
+    Elf_Ehdr E_hdr;
+    Elf_Phdr P_hdr;
+    ramdisk_read(&E_hdr, 0x0, sizeof(Elf_Ehdr));
+    for (int i = 0; i < E_hdr.e_phnum; i++) {
+        ramdisk_read(&P_hdr, E_hdr.e_phoff + i * E_hdr.e_phentsize, E_hdr.e_phentsize);
+        if (P_hdr.p_type == PT_LOAD) {
+            uint32_t tmp[get_ramdisk_size()];
+            ramdisk_read(tmp, P_hdr.p_offset, P_hdr.p_filesz);
+            memcpy((void*)P_hdr.p_vaddr, tmp, P_hdr.p_filesz);
+            memset((void*)(P_hdr.p_vaddr + P_hdr.p_filesz), 0, P_hdr.p_memsz - P_hdr.p_filesz);
+        }
+    }
+    return E_hdr.e_entry;
 }
 
 void naive_uload(PCB* pcb, const char* filename)
