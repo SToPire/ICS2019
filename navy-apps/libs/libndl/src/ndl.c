7c799f16003308a6c81338ca948d5f90fa77bfef
tracer-ics2019 <tracer@njuics.org>
2019-11-26 22:02:03 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  22:02:03 up 11:06,  1 user,  load average: 0.43, 0.54, 0.53 a0db7f61268c78c72d4b5f68c418f98e87d65f6c

--- navy-apps/libs/libndl/src/ndl.c
+++ navy-apps/libs/libndl/src/ndl.c
@@ -138,7 +138,6 @@ int NDL_WaitEvent(NDL_Event* event)
 
 static void get_display_info()
 {
-    assert(0);
     FILE* dispinfo = fopen("/proc/dispinfo", "r");
     // assert(dispinfo);
     // screen_w = screen_h = 0;
