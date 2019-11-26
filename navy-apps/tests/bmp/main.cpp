6ee56ee503bded1547919285ad484b9c4c32820a
tracer-ics2019 <tracer@njuics.org>
2019-11-26 18:41:27 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:41:27 up  7:45,  1 user,  load average: 0.68, 0.90, 0.96 337ef0db92e08cb0687767caf9b90ae1618e5b3b

--- navy-apps/tests/bmp/main.cpp
+++ navy-apps/tests/bmp/main.cpp
@@ -8,9 +8,10 @@ int main()
     NDL_LoadBitmap(bmp, "/share/pictures/projectn.bmp");
     assert(bmp->pixels);
     NDL_OpenDisplay(bmp->w, bmp->h);
-    //NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
-    //NDL_Render();
-    // NDL_CloseDisplay();
-    //while (1);
+    NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
+    NDL_Render();
+    NDL_CloseDisplay();
+    while (1)
+        ;
     return 0;
 }
