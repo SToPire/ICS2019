dcb58ef4dd5d928736ac5709238b010e28f8edc0
tracer-ics2019 <tracer@njuics.org>
2019-11-26 14:29:02 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  14:29:02 up  3:33,  1 user,  load average: 0.13, 0.62, 0.77 e011476a78313bb3146b9736c9119a541251e4f

--- navy-apps/tests/events/events.c
+++ navy-apps/tests/events/events.c
@@ -8,16 +8,19 @@ int main()
     while (1) {
         char buf[256];
         char *p = buf, ch;
+        int i = 0;
         while ((ch = fgetc(fp)) != -1) {
             //printf("%d", ch);
             *p++ = ch;
             //printf("%c", *(p - 1));
             if (ch == '\n') {
+                i++;
                 *p = '\0';
-                printf("%p %p %d\n", p, buf, p - buf);
+                printf("%p %p %d\n", p, buf, i);
                 break;
             }
         }
+        i = 0;
         //printf("%s", buf);
         int is_time = buf[0] == 't';
         time += is_time;
