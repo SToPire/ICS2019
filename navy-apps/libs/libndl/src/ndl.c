c4dc20d3f61f9db8bd1904b7670545d4254f5746
tracer-ics2019 <tracer@njuics.org>
2019-11-26 21:55:26 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  21:55:26 up 10:59,  1 user,  load average: 0.69, 0.59, 0.53 94d32cd5d595cb89a9a2fcad1352620ec53f3bfa

--- navy-apps/libs/libndl/src/ndl.c
+++ navy-apps/libs/libndl/src/ndl.c
@@ -139,16 +139,16 @@ int NDL_WaitEvent(NDL_Event* event)
 static void get_display_info()
 {
     FILE* dispinfo = fopen("/proc/dispinfo", "r");
-    assert(dispinfo);
-    screen_w = screen_h = 0;
-    char buf[128], key[128], value[128], *delim;
-    while (fgets(buf, 128, dispinfo)) {
-        *(delim = strchr(buf, ':')) = '\0';
-        sscanf(buf, "%s", key);
-        sscanf(delim + 1, "%s", value);
-        if (strcmp(key, "WIDTH") == 0) sscanf(value, "%d", &screen_w);
-        if (strcmp(key, "HEIGHT") == 0) sscanf(value, "%d", &screen_h);
-    }
-    fclose(dispinfo);
-    assert(screen_w > 0 && screen_h > 0);
+    // assert(dispinfo);
+    // screen_w = screen_h = 0;
+    // char buf[128], key[128], value[128], *delim;
+    // while (fgets(buf, 128, dispinfo)) {
+    //     *(delim = strchr(buf, ':')) = '\0';
+    //     sscanf(buf, "%s", key);
+    //     sscanf(delim + 1, "%s", value);
+    //     if (strcmp(key, "WIDTH") == 0) sscanf(value, "%d", &screen_w);
+    //     if (strcmp(key, "HEIGHT") == 0) sscanf(value, "%d", &screen_h);
+    // }
+    // fclose(dispinfo);
+    // assert(screen_w > 0 && screen_h > 0);
 }
