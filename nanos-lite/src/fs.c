123a0dba9265c8d411c54b7e0ea944cbca1083e0
tracer-ics2019 <tracer@njuics.org>
2019-11-27 13:58:44 +0800

    >  compile 181860035 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  13:58:44 up  1:01,  1 user,  load average: 1.06, 0.75, 0.66 3bcc9776fe0a8e073d66bcd72ac47938172b1f2d

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,133 +1,136 @@
 #include "fs.h"
 
-size_t serial_write(const void* buf, size_t offset, size_t len);
-size_t ramdisk_read(void* buf, size_t offset, size_t len);
-size_t ramdisk_write(const void* buf, size_t offset, size_t len);
-size_t events_read(void* buf, size_t offset, size_t len);
-size_t fbsync_write(const void* buf, size_t offset, size_t len);
-size_t dispinfo_read(void* buf, size_t offset, size_t len);
-size_t fb_write(const void* buf, size_t offset, size_t len);
-int screen_width();
-int screen_height();
-
-typedef size_t (*ReadFn)(void* buf, size_t offset, size_t len);
-typedef size_t (*WriteFn)(const void* buf, size_t offset, size_t len);
-
+extern size_t serial_write(const void *buf, size_t offset, size_t len);
+extern size_t events_read(void *buf, size_t offset, size_t len);
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+extern size_t get_ramdisk_size();
+typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
+typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
+typedef size_t ssize_t;
+typedef size_t off_t;
 typedef struct {
-    char* name;
-    size_t size;
-    size_t disk_offset;
-    ReadFn read;
-    WriteFn write;
-    size_t open_offset;
+  char *name;
+  size_t size;
+  size_t disk_offset;
+  size_t open_offset;
+  ReadFn read;
+  WriteFn write;
 } Finfo;
 
-enum { FD_STDIN,
-       FD_STDOUT,
-       FD_STDERR,
-       FD_FB };
+enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
 
-size_t invalid_read(void* buf, size_t offset, size_t len)
-{
-    panic("should not reach here");
-    return 0;
+size_t invalid_read(void *buf, size_t offset, size_t len) {
+  panic("should not reach here");
+  return 0;
 }
 
-size_t invalid_write(const void* buf, size_t offset, size_t len)
-{
-    panic("should not reach here");
-    return 0;
+size_t invalid_write(const void *buf, size_t offset, size_t len) {
+  panic("should not reach here");
+  return 0;
 }
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-    {"stdin", 0, 0, invalid_read, invalid_write},
-    {"stdout", 0, 0, invalid_read, serial_write},
-    {"stderr", 0, 0, invalid_read, serial_write},
-    {"/dev/fb", 0, 0, invalid_read, fb_write},
-    {"/dev/events", 0, 0, events_read, invalid_write},
-    {"/dev/fbsync", 0, 0, invalid_read, fbsync_write},
-    {"/proc/dispinfo", 128, 0, dispinfo_read, invalid_write},
+  {"stdin", 0, 0, 0,invalid_read, invalid_write},
+  {"stdout", 0, 0, 0,invalid_read, serial_write},
+  {"stderr", 0, 0, 0,invalid_read, serial_write},
+  {"/dev/fb",0, 0, 0,invalid_read, invalid_write},
+  {"/dev/events",0, 0, 0,events_read,invalid_write},
 #include "files.h"
 };
 
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
 
-void init_fs()
-{
-    file_table[FD_FB].size = screen_height() * screen_width() * 4;
-}
-size_t get_file_size(int fd)
-{
-    return file_table[fd].size;
-}
-size_t get_file_disk_offset(int fd)
-{
-    return file_table[fd].disk_offset;
-}
-int fs_open(const char* pathname, int flags, int mode)
-{
-    for (int i = 0; i < NR_FILES; i++)
-        if (strcmp(file_table[i].name, pathname) == 0) {
-            file_table[i].open_offset = 0;
-            return i;
-        }
-    assert(0);
+void init_fs() {
+  int width=screen_width();
+  int height=screen_height();
+  file_table[FD_FB].size=width*height*4;
+  // 看起来是一个W * H * 4字节的数组，按行优先存储所有像素的颜色值(32位)。每个像素是00rrggbb的形式，8位颜色。屏幕大小从`/proc/dispinfo`文件中获得。
+
+  // TODO: initialize the size of /dev/fb
 }
 
+int fs_open(const char *pathname,int flags,int mode)
+{
+   int mark=0;
+   for(int i=0;i<NR_FILES;i++)
+   {
+      if(strcmp(file_table[i].name,pathname)==0)
+      {
+        mark=i;
+        file_table[i].open_offset=0;
+        Log("I have open the file %d",mark);
+        return mark;
+      }
+   }
+   panic("can't find this file");
+   return -1;
+}
+ssize_t fs_read(int fd,void *buf,size_t len)
+{
+  size_t rlen=len;
+  if(!file_table[fd].read)
+  {
+    if(file_table[fd].open_offset+len>=file_table[fd].size)
+    {
+     rlen=file_table[fd].size-file_table[fd].open_offset;
+     Log("read too much information");
+    }
+    ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,rlen); 
+  }
+  else
+  rlen=file_table[fd].read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,rlen);
+  file_table[fd].open_offset+=rlen;
+ // Log("I have read the file %d with len %d",fd,rlen);
+  return rlen;
+}
 int fs_close(int fd)
 {
-    return 0;
+  Log("I have closed the file %d",fd);
+  return 0;
 }
-
-__ssize_t fs_read(int fd, void* buf, size_t len)
+ssize_t fs_write(int fd,const void *buf,size_t len)
 {
-    Finfo* cur_file = &file_table[fd];
-    if (!cur_file->read) {
-        if (cur_file->open_offset > cur_file->size) return 0;
-        if (len > cur_file->size - cur_file->open_offset)
-            len = cur_file->size - cur_file->open_offset;
-        ramdisk_read(buf, cur_file->disk_offset + cur_file->open_offset, len);
-    } else
-        len = cur_file->read(buf, cur_file->disk_offset + cur_file->open_offset, len);
-    cur_file->open_offset += len;
-    return len;
-}
-
-__ssize_t fs_write(int fd, const void* buf, size_t len)
-{
-    Finfo* cur_file = &file_table[fd];
-    if (!cur_file->write) {
-        if (cur_file->open_offset > cur_file->size) return 0;
-        if (len > cur_file->size - cur_file->open_offset)
-            len = cur_file->size - cur_file->open_offset;
-        ramdisk_write(buf, cur_file->disk_offset + cur_file->open_offset, len);
-    } else
-        len = cur_file->write(buf, cur_file->disk_offset + cur_file->open_offset, len);
-    cur_file->open_offset += len;
-    return len;
-}
-
-__off_t fs_lseek(int fd, size_t offset, int whence)
-{
-    Finfo* cur_file = &file_table[fd];
-    switch (whence) {
-        case SEEK_SET:
-            if (offset >= 0 && offset <= cur_file->size)
-                cur_file->open_offset = offset;
-            else
-                return -1;
-            break;
-        case SEEK_CUR:
-            if (cur_file->open_offset + offset >= 0 && cur_file->open_offset + offset <= cur_file->size)
-                cur_file->open_offset += offset;
-            else
-                return -1;
-            break;
-        case SEEK_END:
-            cur_file->open_offset = cur_file->size + offset;
-            break;
-        default: return (__off_t)(-1);
+  size_t rlen;
+  if(!file_table[fd].write)
+  {
+    if(len+file_table[fd].open_offset>file_table[fd].size)
+    {
+       rlen=file_table[fd].size-file_table[fd].open_offset;
     }
-    return cur_file->open_offset;
+    else rlen=len;
+    ramdisk_write(buf, file_table[fd].disk_offset+file_table[fd].open_offset,rlen);
+  }
+  else
+  rlen = file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+  return rlen;
 }
+off_t fs_lseek(int fd,off_t offset,int whence)
+{
+   Log("I have change the offset %d with len %d",fd,offset);
+   switch(whence)
+   {
+     case SEEK_SET: 
+     {
+        file_table[fd].open_offset=offset;
+        break;
+     }
+     case SEEK_CUR: 
+     {
+        file_table[fd].open_offset+=offset;
+        break;
+     }
+     case SEEK_END: 
+     {
+        file_table[fd].open_offset=file_table[fd].size+offset;
+        break;
+     }
+     default:return -1;
+   }
+   if(file_table[fd].open_offset>file_table[fd].size)
+   file_table[fd].open_offset=file_table[fd].size;
+   return file_table[fd].open_offset;
+}
+  
+
--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,19 +1,20 @@
 #include "common.h"
-int printf(const char* fmt, ...);
-_Context* do_syscall(_Context* c);
-static _Context* do_event(_Event e, _Context* c)
-{
-    switch (e.event) {
-        case _EVENT_SYSCALL: do_syscall(c); break;
-        case _EVENT_YIELD: printf("Hey YIELD!\n"); break;
-        default: panic("Unhandled event ID = %d", e.event);
+_Context* do_syscall(_Context *c);
+static _Context* do_event(_Event e, _Context* c) {
+  switch (e.event) {
+    case _EVENT_YIELD: Log("yield event ID = %d", e.event);break;
+    case _EVENT_SYSCALL: 
+    {
+      do_syscall(c);
+      break;
     }
+    default: panic("Unhandled event ID = %d", e.event);
+  }
 
-    return NULL;
+  return NULL;
 }
 
-void init_irq(void)
-{
-    Log("Initializing interrupt/exception handler...");
-    _cte_init(do_event);
+void init_irq(void) {
+  Log("Initializing interrupt/exception handler...");
+  _cte_init(do_event);
 }
