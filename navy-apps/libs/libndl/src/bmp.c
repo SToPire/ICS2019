17c142a40ec26ff7e3258c4565bf41d7aa9881d7
tracer-ics2019 <tracer@njuics.org>
2019-11-27 15:01:52 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  15:01:52 up  2:04,  1 user,  load average: 0.37, 0.70, 0.70 1e1862d84717d7ccc2dfa44a7185d990fc0bd016

--- navy-apps/libs/libndl/src/bmp.c
+++ navy-apps/libs/libndl/src/bmp.c
@@ -27,7 +27,7 @@ int NDL_LoadBitmap(NDL_Bitmap* bmp, const char* filename)
     w = h = 0;
     if (!(fp = fopen(filename, "r"))) return -1;
     struct BitmapHeader hdr;
-    // assert(sizeof(hdr) == 54);
+    assert(sizeof(hdr) == 54);
     // assert(1 == fread(&hdr, sizeof(struct BitmapHeader), 1, fp));
 
     // if (hdr.bitcount != 24) return -1;
