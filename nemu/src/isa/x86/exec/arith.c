7baabd0fb670bf61bf767469d6e1547bc9afeb23
tracer-ics2019 <tracer@njuics.org>
2019-11-27 21:57:55 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  21:57:55 up  5:56,  1 user,  load average: 0.93, 0.94, 0.89 4744035cea654918c8ca6121217a9e8c7d0efd00

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -34,21 +34,43 @@ make_EHelper(sub)
     print_asm_template2(sub);
 }
 
+// make_EHelper(cmp)
+// {
+//     rtl_sext(&id_src->val, &id_src->val, id_src->width);
+//     rtl_sext(&id_dest->val, &id_dest->val, id_dest->width);
+//     rtl_sub(&s0, &id_dest->val, &id_src->val);
+//     rtl_andi(&s0, &s0, 0xffffffffu >> ((4 - id_dest->width) * 8));
+//     rtl_is_sub_overflow(&s1, &s0, &id_dest->val, &id_src->val, id_dest->width);
+//     rtl_set_OF(&s1);
+//     rtl_is_sub_carry(&s1, &s0, &id_dest->val);
+//     rtl_set_CF(&s1);
+//     rtl_update_ZFSF(&s0, id_dest->width);
+
+//     print_asm_template2(cmp);
+// }
 make_EHelper(cmp)
 {
+    // s0 = dest - src
     rtl_sext(&id_src->val, &id_src->val, id_src->width);
     rtl_sext(&id_dest->val, &id_dest->val, id_dest->width);
     rtl_sub(&s0, &id_dest->val, &id_src->val);
-    rtl_andi(&s0, &s0, 0xffffffffu >> ((4 - id_dest->width) * 8));
-    rtl_is_sub_overflow(&s1, &s0, &id_dest->val, &id_src->val, id_dest->width);
-    rtl_set_OF(&s1);
-    rtl_is_sub_carry(&s1, &s0, &id_dest->val);
-    rtl_set_CF(&s1);
-    rtl_update_ZFSF(&s0, id_dest->width);
+    // s1 = s0
+    s1 = s0;
+    if (id_dest->width != 4) {
+        rtl_andi(&s1, &s1, 0xffffffffu >> ((4 - id_dest->width) * 8));
+    }
 
+    rtl_update_ZFSF(&s1, id_dest->width);
+
+    // update CF
+    rtl_is_sub_carry(&s0, &s1, &id_dest->val);
+    rtl_set_CF(&s0);
+
+    // update OF
+    rtl_is_sub_overflow(&s0, &s1, &id_dest->val, &id_src->val, id_dest->width);
+    rtl_set_OF(&s0);
     print_asm_template2(cmp);
 }
-
 make_EHelper(inc)
 {
     rtl_addi(&t0, &ZERO, 1);
