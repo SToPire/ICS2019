92465645f84969263812a804287b1ecb870bfa99
tracer-ics2019 <tracer@njuics.org>
2019-11-27 13:49:55 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  13:49:55 up 52 min,  1 user,  load average: 0.79, 0.64, 0.60 a52951d6307127214f184663933424332f634e96

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -1,55 +1,54 @@
 #include <am.h>
 #include <amdev.h>
 #include <nemu.h>
-int screen_width();
-int screen_height();
+#define W 400
+#define H 320
 void draw_sync();
-int printf(const char* fmt, ...);
-size_t __am_video_read(uintptr_t reg, void* buf, size_t size)
-{
-    printf("read\n");
-
-    switch (reg) {
-        case _DEVREG_VIDEO_INFO: {
-            _DEV_VIDEO_INFO_t* info = (_DEV_VIDEO_INFO_t*)buf;
-            info->width = 400;
-            info->height = 300;
-            return sizeof(_DEV_VIDEO_INFO_t);
-        }
+size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
+  switch (reg) {
+    case _DEVREG_VIDEO_INFO: {
+      _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
+      info->width = W;
+      info->height = H;
+      return sizeof(_DEV_VIDEO_INFO_t);
     }
-    return 0;
+  }
+  return 0;
 }
 
-size_t __am_video_write(uintptr_t reg, void* buf, size_t size)
-{
-    printf("write\n");
-
-    switch (reg) {
-        case _DEVREG_VIDEO_FBCTL: {
-            _DEV_VIDEO_FBCTL_t* ctl = (_DEV_VIDEO_FBCTL_t*)buf;
-            uint32_t* fb = (uint32_t*)(uintptr_t)FB_ADDR;
-            int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-            uint32_t* pixels = ctl->pixels;
-            int cp_bytes = w < 400 - x ? w : 400 - x;
-            for (int j = 0; j < h && y + j < 300; j++)
-                for (int i = 0; i < cp_bytes; i++)
-                    fb[(y + j) * 400 + x + i] = *pixels++;
-            if (ctl->sync) {
-                outl(SYNC_ADDR, 0);
-            }
-            return size;
-        }
+size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
+  switch (reg) {
+    case _DEVREG_VIDEO_FBCTL: {
+      _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
+      uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+      int x1=ctl->x;
+      int y1=ctl->y;
+      int w1=ctl->w;
+      int h1=ctl->h;
+      uint32_t *pixels1=ctl->pixels;
+      if(x1+w1>W) w1=W-x1;
+      if(y1+h1>W) h1=H-y1;
+      for(int i=0;i<h1;i++)
+      {
+         for(int j=0;j<w1;j++)
+         {
+            *(fb+(y1+i)*W+x1+j)=*(pixels1);
+            pixels1+=1;
+         }
+      }
+      if (ctl->sync) {
+        outl(SYNC_ADDR, 0);
+      }
+      return size;
     }
-    return 0;
+  }
+  return 0;
 }
 
-void __am_vga_init()
-{
-    printf("init\n");
-
-    int i;
-    int size = screen_width() * screen_height();
-    uint32_t* fb = (uint32_t*)(uintptr_t)FB_ADDR;
-    for (i = 0; i < size; i++) fb[i] = 0;
-    draw_sync();
+void __am_vga_init() {
+  int i;
+  int size =H * W;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < size; i ++) fb[i] = i;
+  draw_sync();
 }
