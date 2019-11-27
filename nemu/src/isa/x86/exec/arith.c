0b7422fc6f7cf3b090ebe8d19a30bd72ef8a1178
tracer-ics2019 <tracer@njuics.org>
2019-11-27 22:21:37 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  22:21:37 up  6:20,  1 user,  load average: 1.05, 0.48, 0.54 ffbbb4bafb5f646c63f7b2c3bab1050d5f6768c1

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -54,7 +54,7 @@ make_EHelper(cmp)
     rtl_sext(&id_src->val, &id_src->val, id_src->width);
     rtl_sext(&id_dest->val, &id_dest->val, id_dest->width);
     rtl_sub(&s0, &id_dest->val, &id_src->val);
-    rtl_andi(&s0, &s0, 0xffffffffu >> ((4 - id_dest->width) * 8));
+    // rtl_andi(&s0, &s0, 0xffffffffu >> ((4 - id_dest->width) * 8));
     rtl_is_sub_overflow(&s1, &s0, &id_dest->val, &id_src->val, id_dest->width);
     rtl_set_OF(&s1);
     rtl_is_sub_carry(&s1, &s0, &id_dest->val);
