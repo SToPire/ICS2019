a43475cb4328c42af9281308050719e82703622d
tracer-ics2019 <tracer@njuics.org>
2019-11-27 23:39:53 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  23:39:53 up  7:38,  1 user,  load average: 1.03, 0.52, 0.51 ef620e6ea79e2bd7a6163d4b0b83f7da95485d

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -2,23 +2,33 @@
 
 make_EHelper(add)
 {
-    if (id_src->width != id_dest->width)
-        rtl_sext(&s2, &id_src->val, id_src->width);
-    else
-        rtl_mv(&s2, &id_src->val);
-    rtl_add(&s0, &id_dest->val, &s2);
-    rtl_is_add_overflow(&s1, &s0, &id_dest->val, &s2, id_dest->width);
+    rtl_sext(&id_src->val, &id_src->val, id_src->width);
+    rtl_sext(&id_dest->val, &id_dest->val, id_dest->width);
+    rtl_add(&s0, &id_dest->val, &id_src->val);
+    rtl_is_add_overflow(&s1, &s0, &id_dest->val, &id_src->val, id_dest->width);
     rtl_set_OF(&s1);
     rtl_is_add_carry(&s1, &s0, &id_dest->val);
     rtl_set_CF(&s1);
     rtl_update_ZFSF(&s0, id_dest->width);
-    operand_write(id_dest, &s0);
 
     print_asm_template2(add);
 }
 
 make_EHelper(sub)
 {
+    // if (id_src->width != id_dest->width)
+    //     if(id_dest->width==4)
+    //         rtl_sext(&s2, &id_src->val, id_src->width);
+    //     else if(id_dest->width==2)
+    // else
+    //     rtl_mv(&s2, &id_src->val);
+    // rtl_sub(&s0, &id_dest->val, &s2);
+    // rtl_is_sub_overflow(&s1, &s0, &id_dest->val, &s2, id_dest->width);
+    // rtl_set_OF(&s1);
+    // rtl_is_sub_carry(&s1, &s0, &id_dest->val);
+    // rtl_set_CF(&s1);
+    // rtl_update_ZFSF(&s0, id_dest->width);
+    // operand_write(id_dest, &s0);
     rtl_sext(&id_src->val, &id_src->val, id_src->width);
     rtl_sext(&id_dest->val, &id_dest->val, id_dest->width);
     rtl_sub(&s0, &id_dest->val, &id_src->val);
@@ -27,7 +37,6 @@ make_EHelper(sub)
     rtl_is_sub_carry(&s1, &s0, &id_dest->val);
     rtl_set_CF(&s1);
     rtl_update_ZFSF(&s0, id_dest->width);
-    operand_write(id_dest, &s0);
 
     print_asm_template2(sub);
 }
