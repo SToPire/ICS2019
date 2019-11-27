f89b0c690f46bdf154a11ae9a31a41ea3743d683
tracer-ics2019 <tracer@njuics.org>
2019-11-27 18:46:44 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:46:44 up  2:45,  1 user,  load average: 1.27, 0.88, 0.73 a5048fadc5763099a67b00a84b78faa5bfb0dca0

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -22,27 +22,25 @@ static uintptr_t loader(PCB* pcb, const char* filename)
 {
     int fd = fs_open(filename, 'r', 0);
     //int file_offset = get_file_disk_offset(fd);
-    int file_sz = get_file_size(fd);
+    //int file_sz = get_file_size(fd);
     Elf_Ehdr E_hdr;
-    Elf_Phdr P_hdr;
-    //ramdisk_read(&E_hdr, file_offset, sizeof(Elf_Ehdr));
+    //Elf_Phdr P_hdr;
     fs_read(fd, &E_hdr, sizeof(Elf_Ehdr));
 
-    for (int i = 0; i < E_hdr.e_phnum; i++) {
-        fs_lseek(fd, E_hdr.e_phoff + i * E_hdr.e_phentsize, SEEK_SET);
-        fs_read(fd, &P_hdr, E_hdr.e_phentsize);
-        // ramdisk_read(&P_hdr, file_offset + E_hdr.e_phoff + i * E_hdr.e_phentsize, E_hdr.e_phentsize);
-        if (P_hdr.p_type == PT_LOAD) {
-            uint32_t tmp[file_sz];
-            fs_lseek(fd, P_hdr.p_offset, SEEK_SET);
-            fs_read(fd, tmp, P_hdr.p_filesz);
-            //ramdisk_read(tmp, file_offset + P_hdr.p_offset, P_hdr.p_filesz);
-            //memcpy((uintptr_t*)P_hdr.p_vaddr, tmp, P_hdr.p_filesz);
-            memset((uintptr_t*)(P_hdr.p_vaddr + P_hdr.p_filesz), 0, P_hdr.p_memsz - P_hdr.p_filesz);
-        }
-    }
-    fs_close(fd);
-    return E_hdr.e_entry;
+    // for (int i = 0; i < E_hdr.e_phnum; i++) {
+    //     fs_lseek(fd, E_hdr.e_phoff + i * E_hdr.e_phentsize, SEEK_SET);
+    //     fs_read(fd, &P_hdr, E_hdr.e_phentsize);
+    //     if (P_hdr.p_type == PT_LOAD) {
+    //         uint32_t tmp[file_sz];
+    //         fs_lseek(fd, P_hdr.p_offset, SEEK_SET);
+    //         fs_read(fd, tmp, P_hdr.p_filesz);
+    //         //memcpy((uintptr_t*)P_hdr.p_vaddr, tmp, P_hdr.p_filesz);
+    //         memset((uintptr_t*)(P_hdr.p_vaddr + P_hdr.p_filesz), 0, P_hdr.p_memsz - P_hdr.p_filesz);
+    //     }
+    // }
+    // fs_close(fd);
+    // return E_hdr.e_entry;
+    return 0;
 }
 
 void naive_uload(PCB* pcb, const char* filename)
