9418446dbecb024cd474888011ec8abdb32d4207
tracer-ics2019 <tracer@njuics.org>
2019-11-27 21:40:50 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  21:40:50 up  5:39,  1 user,  load average: 1.31, 0.99, 0.94 f1bf21ea46f24af828def0f2c2c3730a176c5ba3

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -17,24 +17,7 @@ make_EHelper(add)
     print_asm_template2(add);
 }
 
-// make_EHelper(sub)
-// {
-//     if (id_src->width != id_dest->width)
-//         rtl_sext(&s2, &id_src->val, id_src->width);
-//     else
-//         rtl_mv(&s2, &id_src->val);
-//     rtl_sub(&s0, &id_dest->val, &s2);
-//     rtl_is_sub_overflow(&s1, &s0, &id_dest->val, &s2, id_dest->width);
-//     rtl_set_OF(&s1);
-//     rtl_is_sub_carry(&s1, &s0, &id_dest->val);
-//     rtl_set_CF(&s1);
-//     rtl_update_ZFSF(&s0, id_dest->width);
-//     operand_write(id_dest, &s0);
-
-//     print_asm_template2(sub);
-// }
-
-make_EHelper(cmp)
+make_EHelper(sub)
 {
     if (id_src->width != id_dest->width)
         rtl_sext(&s2, &id_src->val, id_src->width);
@@ -46,22 +29,19 @@ make_EHelper(cmp)
     rtl_is_sub_carry(&s1, &s0, &id_dest->val);
     rtl_set_CF(&s1);
     rtl_update_ZFSF(&s0, id_dest->width);
+    operand_write(id_dest, &s0);
 
-    print_asm_template2(cmp);
+    print_asm_template2(sub);
 }
-make_EHelper(sub)
+
+make_EHelper(cmp)
 {
     // s0 = dest - src
-    if ((id_src->width) < (id_dest->width)) {
-        rtl_sext(&id_src->val, &id_src->val, id_src->width);
-        rtl_sext(&id_dest->val, &id_dest->val, id_dest->width);
-    }
+    rtl_sext(&id_src->val, &id_src->val, id_src->width);
+    rtl_sext(&id_dest->val, &id_dest->val, id_dest->width);
     rtl_sub(&s0, &id_dest->val, &id_src->val);
     // s1 = s0
     s1 = s0;
-
-    operand_write(id_dest, &s1);
-
     if (id_dest->width != 4) {
         rtl_andi(&s1, &s1, 0xffffffffu >> ((4 - id_dest->width) * 8));
     }
@@ -69,13 +49,13 @@ make_EHelper(sub)
     rtl_update_ZFSF(&s1, id_dest->width);
 
     // update CF
-    rtl_is_sub_carry(&s0, &s0, &id_dest->val);
+    rtl_is_sub_carry(&s0, &s1, &id_dest->val);
     rtl_set_CF(&s0);
 
     // update OF
     rtl_is_sub_overflow(&s0, &s1, &id_dest->val, &id_src->val, id_dest->width);
     rtl_set_OF(&s0);
-    print_asm_template2(sub);
+    print_asm_template2(cmp);
 }
 
 make_EHelper(inc)
