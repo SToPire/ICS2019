3a3b45aaa43caa7fd4fab3e3da282c40f8d9058f
tracer-ics2019 <tracer@njuics.org>
2019-11-27 14:03:57 +0800

    >  run 181860035 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  14:03:57 up  1:06,  1 user,  load average: 0.70, 0.60, 0.59 d1d6a917b5fcadd1cf2d6e06dd96f956b90791e0

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,68 +1,59 @@
 #include "common.h"
 #include <amdev.h>
-size_t serial_write(const void *buf, size_t offset, size_t len) {
-  char *st=(char*)buf;
-  for(int i=0;i<len;i++)
-  _putc(st[i]);
-  return len;
+int screen_width();
+int screen_height();
+void draw_sync();
+
+size_t serial_write(const void* buf, size_t offset, size_t len)
+{
+    for (int i = 0; i < len; i++)
+        _putc(*(char*)(buf + i));
+    return len;
 }
 
 #define NAME(key) \
-  [_KEY_##key] = #key,
+    [_KEY_##key] = #key,
 
-static const char *keyname[256] __attribute__((used)) = {
-  [_KEY_NONE] = "NONE",
-  _KEYS(NAME)
-};
+static const char* keyname[256] __attribute__((used)) = {
+    [_KEY_NONE] = "NONE",
+    _KEYS(NAME)};
 
-size_t events_read(void *buf, size_t offset, size_t len) {
- // Log("now come to events_read");
-  int keynow=read_key();
-  char temp[256];
-  if(keynow!=0)
-  {
-    if((keynow&0x8000)!=0)
-    {
-      keynow=keynow-0x8000;
-      sprintf(temp,"kd %s\n",keyname[keynow]);
-     // Log("%s",temp);
+size_t events_read(void* buf, size_t offset, size_t len)
+{
+    int getkey = read_key();
+    bool keydown = false;
+    if (getkey & 0x8000) {
+        keydown = true;
+        getkey ^= 0x8000;
     }
+    if (getkey == _KEY_NONE)
+        return snprintf(buf, len, "t %u\n", uptime());
     else
-    {
-      sprintf(temp,"ku %s\n",keyname[keynow]);
-    //  Log("%s",temp);
-    }
-    strncpy(buf,temp,len);
-  }
-  else  
-  {
-    uint32_t nowtime=uptime();
-    sprintf(temp,"t %d\n",nowtime);
-    strncpy(buf,temp,len);
-    //Log("%s",temp);
-  }
-  if(strlen(temp)<len) len=strlen(temp);
-  return len;
+        return snprintf(buf, len, "%s %s\n", keydown ? "kd" : "ku", keyname[getkey]);
 }
 
 static char dispinfo[128] __attribute__((used)) = {};
 
-size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  return 0;
+size_t dispinfo_read(void* buf, size_t offset, size_t len)
+{
+    strncpy(buf, dispinfo + offset, len);
+    return len;
 }
 
-size_t fb_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+size_t fb_write(const void* buf, size_t offset, size_t len)
+{
+    return 0;
 }
 
-size_t fbsync_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+size_t fbsync_write(const void* buf, size_t offset, size_t len)
+{
+    draw_sync();
+    return 0;
 }
 
-void init_device() {
-  Log("Initializing devices...");
-  _ioe_init();
-
-  // TODO: print the string to array `dispinfo` with the format
-  // described in the Navy-apps convention
+void init_device()
+{
+    Log("Initializing devices...");
+    _ioe_init();
+    sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d", screen_width(), screen_height());
 }
