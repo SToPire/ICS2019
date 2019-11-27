b031fe351b03196c258bf74e825ae9f3e373d7ae
tracer-ics2019 <tracer@njuics.org>
2019-11-27 21:34:40 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  21:34:40 up  5:33,  1 user,  load average: 1.10, 0.81, 0.88 c2c4808b77bbc580ab383f3d4442d43db9193ea2

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -1,18 +1,42 @@
 #include "cpu/exec.h"
 
+// make_EHelper(add)
+// {
+//     if (id_src->width != id_dest->width)
+//         rtl_sext(&s2, &id_src->val, id_src->width);
+//     else
+//         rtl_mv(&s2, &id_src->val);
+//     rtl_add(&s0, &id_dest->val, &s2);
+//     rtl_is_add_overflow(&s1, &s0, &id_dest->val, &s2, id_dest->width);
+//     rtl_set_OF(&s1);
+//     rtl_is_add_carry(&s1, &s0, &id_dest->val);
+//     rtl_set_CF(&s1);
+//     rtl_update_ZFSF(&s0, id_dest->width);
+//     operand_write(id_dest, &s0);
+
+//     print_asm_template2(add);
+// }
 make_EHelper(add)
 {
-    if (id_src->width != id_dest->width)
-        rtl_sext(&s2, &id_src->val, id_src->width);
-    else
-        rtl_mv(&s2, &id_src->val);
-    rtl_add(&s0, &id_dest->val, &s2);
-    rtl_is_add_overflow(&s1, &s0, &id_dest->val, &s2, id_dest->width);
-    rtl_set_OF(&s1);
-    rtl_is_add_carry(&s1, &s0, &id_dest->val);
-    rtl_set_CF(&s1);
-    rtl_update_ZFSF(&s0, id_dest->width);
-    operand_write(id_dest, &s0);
+    // s0 = dest + src
+    rtl_add(&s0, &id_dest->val, &id_src->val);
+    // s1 = s0
+    s1 = s0;
+    operand_write(id_dest, &s1);
+
+    if (id_dest->width != 4) {
+        rtl_andi(&s1, &s1, 0xffffffffu >> ((4 - id_dest->width) * 8));
+    }
+
+    rtl_update_ZFSF(&s1, id_dest->width);
+
+    // update CF
+    rtl_is_add_carry(&s0, &s0, &id_dest->val);
+    rtl_set_CF(&s0);
+
+    // update OF
+    rtl_is_add_overflow(&s0, &s1, &id_dest->val, &id_src->val, id_dest->width);
+    rtl_set_OF(&s0);
 
     print_asm_template2(add);
 }
