aa078ade01f62dcf9d010c417e5ff893231b0bfc
tracer-ics2019 <tracer@njuics.org>
2019-11-27 16:52:42 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  16:52:42 up 51 min,  1 user,  load average: 0.44, 0.31, 0.28 aded21677109dc22b6d1f0b1c6358b34652a494

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -8,45 +8,44 @@
 #    define Elf_Ehdr Elf32_Ehdr
 #    define Elf_Phdr Elf32_Phdr
 #endif
-size_t ramdisk_read(void* buf, size_t offset, size_t len);
-size_t get_file_disk_offset(int fd);
-size_t get_file_size(int fd);
-
-int fs_open(const char* pathname, int flags, int mode);
-int fs_close(int fd);
-__ssize_t fs_read(int fd, void* buf, size_t len);
-__off_t fs_lseek(int fd, size_t offset, int whence);
-
+extern size_t ramdisk_read(void* buf, size_t offset, size_t len);
+extern size_t ramdisk_write(const void* buf, size_t offset, size_t len);
+extern size_t get_ramdisk_size();
+extern int fs_close(int fd);
+extern size_t fs_read(int fd, void* buf, size_t len);
+extern int fs_open(const char* pathname, int flags, int mode);
+extern size_t fs_write(int fd, const void* buf, size_t len);
+extern size_t fs_lseek(int fd, size_t offset, int whence);
 static uintptr_t loader(PCB* pcb, const char* filename)
 {
-    int fd = fs_open(filename, 'r', 0);
-    //int file_offset = get_file_disk_offset(fd);
-    int file_sz = get_file_size(fd);
-    Elf_Ehdr E_hdr;
-    Elf_Phdr P_hdr;
-    //ramdisk_read(&E_hdr, file_offset, sizeof(Elf_Ehdr));
-    fs_read(fd, &E_hdr, sizeof(Elf_Ehdr));
-
-    for (int i = 0; i < E_hdr.e_phnum; i++) {
-        fs_lseek(fd, E_hdr.e_phoff + i * E_hdr.e_phentsize, SEEK_SET);
-        fs_read(fd, &P_hdr, E_hdr.e_phentsize);
-        // ramdisk_read(&P_hdr, file_offset + E_hdr.e_phoff + i * E_hdr.e_phentsize, E_hdr.e_phentsize);
-        if (P_hdr.p_type == PT_LOAD) {
-            uint32_t tmp[file_sz];
-            fs_lseek(fd, P_hdr.p_offset, SEEK_SET);
-            fs_read(fd, tmp, P_hdr.p_filesz);
-            //ramdisk_read(tmp, file_offset + P_hdr.p_offset, P_hdr.p_filesz);
-            memcpy((void*)P_hdr.p_vaddr, tmp, P_hdr.p_filesz);
-            memset((void*)(P_hdr.p_vaddr + P_hdr.p_filesz), 0, P_hdr.p_memsz - P_hdr.p_filesz);
+    Elf_Ehdr elf_header;
+    Elf_Phdr pro_header;
+    int fd = fs_open(filename, 0, 0);
+    fs_read(fd, &elf_header, sizeof(Elf_Ehdr));
+    size_t phoffset = elf_header.e_phoff;
+    size_t offset = phoffset;
+    size_t phnum = elf_header.e_phnum;
+    size_t phentsize = elf_header.e_phentsize;
+    fs_lseek(fd, offset, SEEK_SET);
+    for (int i = 0; i < phnum; i++) {
+        fs_read(fd, &pro_header, phentsize);
+        if (pro_header.p_type == PT_LOAD) {
+            fs_lseek(fd, pro_header.p_offset, SEEK_SET);
+            fs_read(fd, (uintptr_t*)pro_header.p_vaddr, pro_header.p_filesz);
+            memset((uintptr_t*)(pro_header.p_vaddr + pro_header.p_filesz), 0, pro_header.p_memsz - pro_header.p_filesz);
         }
+        offset += phentsize;
+        if (i < phnum - 1)
+            fs_lseek(fd, offset, SEEK_SET);
     }
     fs_close(fd);
-    return E_hdr.e_entry;
+    return elf_header.e_entry;
 }
 
 void naive_uload(PCB* pcb, const char* filename)
 {
-    uintptr_t entry = loader(pcb, filename);
+    uintptr_t entry;
+    entry = loader(pcb, filename);
     Log("Jump to entry = %x", entry);
     ((void (*)())entry)();
 }
