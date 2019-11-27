a00103714f68bc5407a34cc38c6c66c7f08fb2fc
tracer-ics2019 <tracer@njuics.org>
2019-11-27 10:43:17 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  10:43:17 up 23:47,  1 user,  load average: 0.74, 0.65, 0.68 59fef8cb8f50c2ea31118d8cbfa77e58c6eea70b

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -5,107 +5,102 @@
 // extern int screen_height();
 // extern void draw_rect(uint32_t *pixels, int x, int y, int w, int h);
 // extern size_t fs_filesz(int fd);
-extern int fs_open(const char* pathname, int flags, int mode);
-extern size_t fs_read(int fd, void* buf, size_t len);
-extern size_t fs_write(int fd, const void* buf, size_t len);
+extern int fs_open(const char *pathname, int flags, int mode);
+extern size_t fs_read(int fd, void *buf, size_t len);
+extern size_t fs_write(int fd, const void *buf, size_t len);
 extern size_t fs_lseek(int fd, size_t offset, int whence);
 extern int fs_close(int fd);
 
-size_t serial_write(const void* buf, size_t offset, size_t len)
-{
-    int ret = 0;
-    for (; ret < len; ret++) _putc(((char*)buf)[ret]);
-    return ret;
+size_t serial_write(const void *buf, size_t offset, size_t len) {
+  int ret=0;
+  for (;ret<len;ret++) _putc(((char*)buf)[ret]);
+  return ret;
 }
 
 #define NAME(key) \
-    [_KEY_##key] = #key,
+  [_KEY_##key] = #key,
 
-static const char* keyname[256] __attribute__((used)) = {
-    [_KEY_NONE] = "NONE",
-    _KEYS(NAME)};
+static const char *keyname[256] __attribute__((used)) = {
+  [_KEY_NONE] = "NONE",
+  _KEYS(NAME)
+};
 
-size_t events_read(void* buf, size_t offset, size_t len)
-{
-    char temp[1024];
-    int key = read_key();
+size_t events_read(void *buf, size_t offset, size_t len) {
+  char temp[1024];
+  int key = read_key();
+  
+  bool down = false;
+  if (key & 0x8000) {
+    key ^= 0x8000;
+    down = true;
+  }
 
-    bool down = false;
-    if (key & 0x8000) {
-        key ^= 0x8000;
-        down = true;
-    }
-
-    if (key == _KEY_NONE) {
-        uint32_t t = uptime();
-        sprintf(temp, "t %d\n", t);
-    } else {
-        if (down)
-            sprintf(temp, "%s %s\n", "kd", keyname[key]);
-        else
-            sprintf(temp, "%s %s\n", "ku", keyname[key]);
-    }
-    if (strlen(temp) < len) len = strlen(temp);
-
-    memcpy(buf, temp, len);
-    return len;
+  if (key == _KEY_NONE) {
+    uint32_t t = uptime();
+    sprintf(temp, "t %d\n", t);
+  } else {
+    if (down)
+      sprintf(temp, "%s %s\n", "kd", keyname[key]);
+    else
+      sprintf(temp, "%s %s\n", "ku", keyname[key]);
+  }
+  if (strlen(temp)<len) len = strlen(temp);
+  
+  memcpy(buf, temp, len);
+  return len;
 }
 
 static char dispinfo[128] __attribute__((used)) = {};
 
-size_t dispinfo_read(void* buf, size_t offset, size_t len)
-{
-    // printf("offset = %d, len = %d\n",offset, len);
-    int filesz = 128;
-    if (len + offset > filesz) len = filesz - offset;
-    // printf("offset = %d, len = %d\n",offset, len);
-    strncpy(buf, dispinfo + offset, len);
-    // printf("%d %d\n",screen_width(),screen_height());
-    return len;
+size_t dispinfo_read(void *buf, size_t offset, size_t len) {
+  // printf("offset = %d, len = %d\n",offset, len);
+  int filesz = 128;
+  if (len+offset>filesz) len=filesz-offset;
+  // printf("offset = %d, len = %d\n",offset, len);
+  strncpy(buf, dispinfo+offset, len);
+  // printf("%d %d\n",screen_width(),screen_height());
+  return len;
 }
 
-size_t fb_write(const void* buf, size_t offset, size_t len)
-{
-    int x, y;
-    offset >>= 2;
-    int scr_width = screen_width();
-    // int scr_height = screen_height();
-    x = offset % scr_width;
-    y = offset / scr_width;
+size_t fb_write(const void *buf, size_t offset, size_t len) {
+  int x, y;
+  offset >>= 2;
+  int scr_width = screen_width();
+  // int scr_height = screen_height();
+  x = offset%scr_width;
+  y = offset/scr_width;
 
-    // printf("fb write: x=%d, y=%d\n",x,y);
+  // printf("fb write: x=%d, y=%d\n",x,y);
 
-    int res = len;
+  int res = len;
 
-    len >>= 2;
-    int len_t = len < scr_width - x ? len : scr_width - x;
-    draw_rect((uint32_t*)buf, x, y, len_t, 1);
+  len >>= 2;
+  int len_t = len<scr_width-x? len: scr_width-x;
+  draw_rect((uint32_t*)buf, x, y, len_t, 1);
 
-    if (len_t < len - scr_width && len_t < len) {
-        draw_rect((uint32_t*)buf + len_t, 0, y + 1, scr_width, (len - len_t) / scr_width);
-        len_t += scr_width * ((len - len_t) / scr_width);
-        y += (len - len_t) / scr_width;
-    }
+  if (len_t<len-scr_width && len_t<len) {
+    draw_rect((uint32_t*)buf+len_t, 0, y+1, scr_width, (len-len_t)/scr_width);
+    len_t += scr_width*((len-len_t)/scr_width);
+    y += (len-len_t)/scr_width;
+  } 
 
-    if (len_t < len) {
-        draw_rect((uint32_t*)buf + len_t, 0, y + 1, len - len_t, 1);
-    }
-
-    return res;
+  if (len_t < len) {
+    draw_rect((uint32_t*)buf+len_t, 0, y+1, len-len_t, 1);
+  }
+  
+  return res;
 }
 
-size_t fbsync_write(const void* buf, size_t offset, size_t len)
-{
-    draw_sync();
-    return len;
+size_t fbsync_write(const void *buf, size_t offset, size_t len) {
+  draw_sync();
+  return len;
 }
 
-void init_device()
-{
-    Log("Initializing devices...");
-    _ioe_init();
+void init_device() {
+  Log("Initializing devices...");
+  _ioe_init();
 
-    // TODO: print the string to array `dispinfo` with the format
-    // described in the Navy-apps convention
-    sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d", screen_width(), screen_height());
+  // TODO: print the string to array `dispinfo` with the format
+  // described in the Navy-apps convention
+  sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d", screen_width(), screen_height());
 }
