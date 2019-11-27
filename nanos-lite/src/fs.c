fca888bd7d2a813f8f7ddcb52bb26b630bf2029d
tracer-ics2019 <tracer@njuics.org>
2019-11-27 10:33:33 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  10:33:33 up 23:37,  1 user,  load average: 0.89, 0.96, 0.79 cb26a7dc95fedcc317d7b5561683259b7bfbe886

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -84,8 +84,6 @@ int fs_close(int fd)
 
 __ssize_t fs_read(int fd, void* buf, size_t len)
 {
-    printf("read\n");
-
     Finfo* cur_file = &file_table[fd];
     if (!cur_file->read) {
         if (cur_file->open_offset > cur_file->size) return 0;
@@ -100,8 +98,6 @@ __ssize_t fs_read(int fd, void* buf, size_t len)
 
 __ssize_t fs_write(int fd, const void* buf, size_t len)
 {
-    printf("write\n");
-
     Finfo* cur_file = &file_table[fd];
     if (!cur_file->write) {
         if (cur_file->open_offset > cur_file->size) return 0;
@@ -116,8 +112,6 @@ __ssize_t fs_write(int fd, const void* buf, size_t len)
 
 __off_t fs_lseek(int fd, size_t offset, int whence)
 {
-    printf("lseek\n");
-
     Finfo* cur_file = &file_table[fd];
     switch (whence) {
         case SEEK_SET:
