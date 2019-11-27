85016e0e4cfae5915e7a770d0e06c176fa871ad0
tracer-ics2019 <tracer@njuics.org>
2019-11-28 00:35:12 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  00:35:12 up  8:33,  1 user,  load average: 1.08, 0.79, 0.59 43661fd440dee2e5bca71dc04be1121c83814989

--- nemu/src/isa/x86/reg.c
+++ nemu/src/isa/x86/reg.c
@@ -29,6 +29,7 @@ void reg_test() {
   assert(reg_b(R_DH) == ((sample[R_EDX] >> 8) & 0xff));
 
   assert(sample[R_EAX] == cpu.eax);
+  assert(sample[R_ECX] == cpu.ecx);
   assert(sample[R_EDX] == cpu.edx);
   assert(sample[R_EBX] == cpu.ebx);
   assert(sample[R_ESP] == cpu.esp);
@@ -40,44 +41,32 @@ void reg_test() {
 }
 
 void isa_reg_display() {
-  int i;
-  for(i=R_EAX;i<=R_EDI;i++)
-  {
-    printf("%s %d\n",regsl[i],reg_l(i));
-  }
-  printf("CF %d\n",cpu.EFLAGS.CF);
-  printf("OF %d\n",cpu.EFLAGS.OF);
-  printf("SF %d\n",cpu.EFLAGS.SF);
-  printf("ZF %d\n",cpu.EFLAGS.ZF);
+	printf("%-8s0x%08x %d\n","%eax:",cpu.eax,cpu.eax);
+	printf("%-8s0x%08x %d\n","%ecx:",cpu.ecx,cpu.ecx);
+	printf("%-8s0x%08x %d\n","%edx:",cpu.edx,cpu.edx);
+	printf("%-8s0x%08x %d\n","%ebx:",cpu.ebx,cpu.ebx);
+	printf("%-8s0x%08x %d\n","%esp:",cpu.esp,cpu.esp);
+	printf("%-8s0x%08x %d\n","%ebp:",cpu.ebp,cpu.ebp);
+	printf("%-8s0x%08x %d\n","%esi:",cpu.esi,cpu.esi);
+	printf("%-8s0x%08x %d\n","%edi:",cpu.edi,cpu.edi);
 }
 
 uint32_t isa_reg_str2val(const char *s, bool *success) {
-  int bj=0;
-  uint32_t re;
-  for(int i=R_EAX;i<=R_EDI;i++)
-  {  
-    if(strcmp(regsl[i],s)==0)
-    {
-      bj=1;
-      re=reg_l(i);
-    }
-    else
-    if(strcmp(regsw[i],s)==0)
-    {
-      bj=1;
-      re=reg_w(i);
-    }
-    else
-    if(strcmp(regsb[i],s)==0)
-    {
-      bj=1;
-      re=reg_b(i);
-    }
-  }
-  if(bj==0) 
-  {
-    *success=false;
-    return 1;
-  }
-  else return re;
+	int i,l=strlen(s);
+	*success = true;
+	char tmp[10]="";strcpy(tmp,s);
+	for(i=0;i<l;i++) tmp[i]=tolower(s[i]);
+	if(strcmp(tmp,"pc")==0) return cpu.pc;
+	else if(strcmp(tmp,"cf")==0) return cpu.eflags.CF; 
+	else if(strcmp(tmp,"zf")==0) return cpu.eflags.ZF;
+	else if(strcmp(tmp,"of")==0) return cpu.eflags.OF;
+	else if(strcmp(tmp,"sf")==0) return cpu.eflags.SF;
+	else if(strcmp(tmp,"if")==0) return cpu.eflags.IF;
+	for(i=0;i<8;i++){
+		if(strcmp(tmp,reg_name(i,1))==0) return reg_b(i);
+		if(strcmp(tmp,reg_name(i,2))==0) return reg_w(i);
+		if(strcmp(tmp,reg_name(i,4))==0) return reg_l(i);
+	}	
+	*success = false;
+	return 0;
 }
