eea13d76d0f2143e50e0de0881b41884f30535ec
tracer-ics2019 <tracer@njuics.org>
2019-11-28 17:31:10 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:31:10 up  1:22,  1 user,  load average: 1.29, 1.03, 1.24 909adb9187c0da30d7b66a5df053a2265810eebd

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -11,18 +11,18 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     CC_L, CC_NL, CC_LE, CC_NLE
   };
 
-  // TODO: Query eflags to determine whether the condition code is satisfied.
+  // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:*dest=(cpu.eflags.OF==1);break;
-    case CC_B:*dest=(cpu.eflags.CF==1);break;
-    case CC_E:*dest=(cpu.eflags.ZF==1);break;
-    case CC_BE:*dest=((cpu.eflags.CF==1)||(cpu.eflags.ZF==1));break;
-    case CC_S:*dest=(cpu.eflags.SF==1);break;
-    case CC_L:*dest=(cpu.eflags.SF!=cpu.eflags.OF);break;
-    case CC_LE:*dest=((cpu.eflags.SF!=cpu.eflags.OF)||(cpu.eflags.ZF==1));break;
-    default: panic("should not reach here");break;
-    case CC_P: panic("n86 does not have PF");break;
+    case CC_O:*dest=(cpu.eflags.OF)?1:0;break;
+    case CC_B:*dest=(cpu.eflags.CF)?1:0;break;
+    case CC_E:*dest=(cpu.eflags.ZF)?1:0;break;
+    case CC_BE:*dest=(cpu.eflags.CF || cpu.eflags.ZF)?1:0;break;
+    case CC_S:*dest=(cpu.eflags.SF)?1:0;break;
+    case CC_L:*dest=(cpu.eflags.SF != cpu.eflags.OF)?1:0;break;
+    case CC_LE:*dest=((cpu.eflags.SF != cpu.eflags.OF) || cpu.eflags.ZF)?1:0;break;
+    default: panic("should not reach here");
+    case CC_P: panic("n86 does not have PF");
   }
 
   if (invert) {
