29f24f34cd2df6cf08dc06e3e0cffcb2de042594
tracer-ics2019 <tracer@njuics.org>
2019-12-02 19:30:57 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:30:57 up  1:12,  1 user,  load average: 0.79, 0.52, 0.47 45df82cd5a1ee73ff1cba9ce2e0c3d2693a9a7

--- nexus-am/am/src/x86/nemu/cte.c
+++ nexus-am/am/src/x86/nemu/cte.c
@@ -55,8 +55,8 @@ int _cte_init(_Context* (*handler)(_Event, _Context*))
 _Context* _kcontext(_Area stack, void (*entry)(void*), void* arg)
 {
     printf("%p %p\n", stack.start, stack.end);
-    _Context tmp;
-    printf("%d\n", sizeof(tmp));
+    _Context* tmp = stack.end - sizeof(_Context);
+    printf("%p\n", tmp);
     return NULL;
 }
 
