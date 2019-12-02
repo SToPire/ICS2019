40a7c51c58eb7cfe07de3d0002a65c8634871f09
tracer-ics2019 <tracer@njuics.org>
2019-12-03 00:32:56 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  00:32:56 up  6:14,  1 user,  load average: 0.42, 0.39, 0.41 935deb87d91a0b16ae615a92fcc47071efe87738

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,7 +27,7 @@ void hello_fun(void* arg)
 void init_proc()
 {
     //naive_uload(NULL, "/bin/init");
-    //context_kload(&pcb[0], (void*)hello_fun);
+    context_kload(&pcb[0], (void*)hello_fun);
     context_uload(&pcb[1], "/bin/init");
     switch_boot_pcb();
     Log("Initializing processes...");
@@ -38,7 +38,7 @@ _Context* schedule(_Context* prev)
     current->cp = prev;
     Log("%d", current == &pcb[0]);
 
-    current = (current == &pcb[1] ? &pcb[0] : &pcb[1]);
+    current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
     Log("%d", current == &pcb[0]);
     Log("%x", pcb[1].cp->eip);
     return current->cp;
