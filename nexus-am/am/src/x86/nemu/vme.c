6bd5ac64e3b4be48f7faabf842086eff10a0e907
tracer-ics2019 <tracer@njuics.org>
2019-12-03 00:50:05 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  00:50:05 up  6:31,  1 user,  load average: 0.81, 0.50, 0.42 5103732ac0ca46a4464b86f2cd15aa88e9ea92f

--- nexus-am/am/src/x86/nemu/vme.c
+++ nexus-am/am/src/x86/nemu/vme.c
@@ -91,7 +91,9 @@ int printf(const char* fmt, ...);
 
 _Context* _ucontext(_AddressSpace* as, _Area ustack, _Area kstack, void* entry, void* args)
 {
-    _Context* tmp = ustack.end - sizeof(_Context);
+    uintptr_t* tp = ustack.end - 3 * sizeof(uintptr_t);
+    *(tp) = *(tp + 1) = *(tp + 2) = 0;
+    _Context* tmp = ustack.end - 3 * sizeof(uintptr_t) - sizeof(_Context);
     tmp->cs = 0x8;
     tmp->eip = (uintptr_t)entry;
     return tmp;
