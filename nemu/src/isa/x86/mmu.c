7588d0e4647e59f7fabd7cf1dea332c3ca62d93e
tracer-ics2019 <tracer@njuics.org>
2019-12-15 21:46:27 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-37-generic #40~18.04.1-Ubuntu SMP Thu Nov 14 12:06:39 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  21:46:27 up  2:08,  1 user,  load average: 0.43, 0.42, 0.55 4517bba6a5ef495b7c3712c42412cdca601831ea

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -1,10 +1,23 @@
 #include "nemu.h"
 #include <stdio.h>
-uint32_t isa_vaddr_read(vaddr_t addr, int len) {
-    if (cpu.cr0.paging) printf("cr3:%x\n",cpu.cr3.page_directory_base);
-    return paddr_read(addr, len);
+paddr_t page_translate(vaddr_t addr);
+
+uint32_t isa_vaddr_read(vaddr_t addr, int len)
+{
+    if (cpu.cr0.paging){
+        paddr_t paddr = page_translate(addr);
+        return paddr_read(paddr, len);
+    }else{
+        return paddr_read(addr, len);
+    }
 }
 
 void isa_vaddr_write(vaddr_t addr, uint32_t data, int len) {
   paddr_write(addr, data, len);
+}
+
+paddr_t page_translate(vaddr_t addr)
+{
+    printf("%x\n", cpu.cr3.page_directory_base);
+    return addr;
 }
