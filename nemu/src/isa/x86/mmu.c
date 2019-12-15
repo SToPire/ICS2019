eb7613f3145beff6cd2ee38a0c12f85b88276f1a
tracer-ics2019 <tracer@njuics.org>
2019-12-16 00:34:28 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-37-generic #40~18.04.1-Ubuntu SMP Thu Nov 14 12:06:39 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  00:34:28 up  4:56,  1 user,  load average: 0.68, 0.84, 0.94 db965a8bf68e1e36f01048e7aa1c2eabcca7fec4

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -16,38 +16,39 @@ uint32_t get_OFFSET(vaddr_t addr)
 }
 uint32_t isa_vaddr_read(vaddr_t addr, int len)
 {
-    // if (cpu.cr0.paging) {
-    //     if (get_OFFSET(addr) + len > PAGE_SIZE) {
-    //         int s1 = PAGE_SIZE - get_OFFSET(addr);
-    //         int s2 = len - s1;
-    //         uint32_t t1 = paddr_read(page_translate(addr), s1);
-    //         uint32_t t2 = paddr_read(page_translate(addr + len), s2);
-    //         return (t1 << (8 * s2)) | t2;
-    //     } else {
-    //         paddr_t paddr = page_translate(addr);
-    //         return paddr_read(paddr, len);
-    //     }
-    // } else {
-       return paddr_read(addr, len);
-    // }
+    if (cpu.cr0.paging) {
+        if (get_OFFSET(addr) + len > PAGE_SIZE) {
+            int s1 = PAGE_SIZE - get_OFFSET(addr);
+            int s2 = len - s1;
+            uint32_t t1 = paddr_read(page_translate(addr), s1);
+            uint32_t t2 = paddr_read(page_translate(addr + s1), s2);
+            printf("%x %x\n", t1, t2);
+            return (t1 << (8 * s2)) | t2;
+        } else {
+            paddr_t paddr = page_translate(addr);
+            return paddr_read(paddr, len);
+        }
+    } else {
+        return paddr_read(addr, len);
+    }
 }
 
 void isa_vaddr_write(vaddr_t addr, uint32_t data, int len)
 {
-//     if (cpu.cr0.paging) {
-//         if (get_OFFSET(addr) + len > PAGE_SIZE) {
-//             int s1 = PAGE_SIZE - get_OFFSET(addr);
-//             int s2 = len - s1;
-//             paddr_write(page_translate(addr), data >> (8 * s2), s1);
-//             paddr_write(page_translate(addr + len), data << (8 * s1) >> (8 * s1), s2);
-// //            assert(0);
-//         } else {
-//             paddr_t paddr = page_translate(addr);
-//             paddr_write(paddr, data, len);
-//         }
-//     } else {
+    if (cpu.cr0.paging) {
+        if (get_OFFSET(addr) + len > PAGE_SIZE) {
+            int s1 = PAGE_SIZE - get_OFFSET(addr);
+            int s2 = len - s1;
+            paddr_write(page_translate(addr), data >> (8 * s2), s1);
+            paddr_write(page_translate(addr + s1), data << (8 * s1) >> (8 * s1), s2);
+//            assert(0);
+        } else {
+            paddr_t paddr = page_translate(addr);
+            paddr_write(paddr, data, len);
+        }
+    } else {
         paddr_write(addr, data, len);
-//     }
+    }
 }
 
 paddr_t page_translate(vaddr_t addr)
