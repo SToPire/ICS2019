2e53b18c34a7df5d57a1fdee49e5bd64180ad44c
tracer-ics2019 <tracer@njuics.org>
2019-12-16 18:45:37 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-37-generic #40~18.04.1-Ubuntu SMP Thu Nov 14 12:06:39 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:45:37 up  8:24,  1 user,  load average: 0.64, 0.73, 0.72 87d49201e253a10a44670ae7e9c9d62fd99f5bf

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,7 +28,7 @@ void init_proc()
 {
     //naive_uload(NULL, "/bin/init");
     //context_kload(&pcb[0], (void*)hello_fun);
-    context_uload(&pcb[1], "/bin/pal");
+    context_uload(&pcb[1], "/bin/bmptest");
     switch_boot_pcb();
     Log("Initializing processes...");
 }
