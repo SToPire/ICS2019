54253e6dc59b502392730526180ae1e72dc4519b
tracer-ics2019 <tracer@njuics.org>
2019-12-17 19:22:59 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-37-generic #40~18.04.1-Ubuntu SMP Thu Nov 14 12:06:39 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:22:59 up  3:03,  1 user,  load average: 1.75, 1.16, 1.07 a498f7e017f67d3437922aa19b71002cb2efe5e7

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -8,7 +8,7 @@ static _Context* do_event(_Event e, _Context* c)
         case _EVENT_YIELD:
             return schedule(c);
         case _EVENT_IRQ_TIMER:
-            Log("Timer interrupt");
+            //Log("Timer interrupt");
             _yield();
             break;
         default: panic("Unhandled event ID = %d", e.event);
