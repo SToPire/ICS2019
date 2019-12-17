53f5c375ae616edb381b2e7bf4f6c22fab635eba
tracer-ics2019 <tracer@njuics.org>
2019-12-17 18:15:54 +0800

    >  compile 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-37-generic #40~18.04.1-Ubuntu SMP Thu Nov 14 12:06:39 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:15:54 up  1:56,  1 user,  load average: 1.13, 1.25, 1.34 22c46f04734656bb6ffc627f8caeddde1f1777f2

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -22,6 +22,7 @@ void raise_intr(uint32_t NO, vaddr_t ret_addr)
 bool isa_query_intr(void)
 {
     if (cpu.INTR) {
+        Log(":in");
         cpu.INTR = false;
         raise_intr(IRQ_TIMER, decinfo.seq_pc);
         return true;
