3964c7a90478109dfdcb81a421c7232fa792c2fe
tracer-ics2019 <tracer@njuics.org>
2019-12-17 19:32:48 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-37-generic #40~18.04.1-Ubuntu SMP Thu Nov 14 12:06:39 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:32:48 up  3:13,  1 user,  load average: 0.74, 1.18, 1.24 2904594e2c9a19d750adc3c43d5365b66e47989

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -6,7 +6,6 @@ void draw_sync();
 
 size_t serial_write(const void* buf, size_t offset, size_t len)
 {
-    _yield();
     for (int i = 0; i < len; i++)
         _putc(*(char*)(buf + i));
     return len;
@@ -21,7 +20,6 @@ static const char* keyname[256] __attribute__((used)) = {
 
 size_t events_read(void* buf, size_t offset, size_t len)
 {
-    _yield();
     int getkey = read_key();
     bool keydown = false;
     if (getkey & 0x8000) {
@@ -46,7 +44,6 @@ size_t dispinfo_read(void* buf, size_t offset, size_t len)
 
 size_t fb_write(const void* buf, size_t offset, size_t len)
 {
-    _yield();
     int offset_B = offset / 4;
     int len_B = len / 4;
     int w = screen_width();
