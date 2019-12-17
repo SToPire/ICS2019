ab39d2ef5516ed01a2a174a1b5aa1599e011ff78
tracer-ics2019 <tracer@njuics.org>
2019-12-17 13:01:52 +0800

    >  run 181250195 zyf Linux zyf-ThinkPad-E480 5.0.0-37-generic #40~18.04.1-Ubuntu SMP Thu Nov 14 12:06:39 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  13:01:52 up 9 min,  1 user,  load average: 0.68, 0.48, 0.24 f7c5a17288ef07e2ce12857749a1b6bec81ab1

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -28,6 +28,7 @@ static uintptr_t loader(PCB* pcb, const char* filename)
         if (P_hdr.p_type == PT_LOAD) {
             fs_lseek(fd, P_hdr.p_offset, SEEK_SET);
             void* vaddr = (void*)P_hdr.p_vaddr;
+            printf("fsz:%x msz:%x\n", P_hdr.p_filesz, P_hdr.p_memsz);
             for (uint32_t i = 0; i < P_hdr.p_filesz; i += PGSIZE) {
                 void* paddr = new_page(1);
                 uint32_t sz = (P_hdr.p_filesz - i >= PGSIZE) ? PGSIZE : (P_hdr.p_filesz - i);
